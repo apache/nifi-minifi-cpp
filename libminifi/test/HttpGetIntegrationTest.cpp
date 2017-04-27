35e24a38818108098a9f3ead17240778f57294b8
Aldrin Piri <aldrin@apache.org>
2017-04-27 14:26:48 -0400

    MINIFI-274 Use external project with existing source for jsoncpp to avoid contamination of core project build.
    Adding needed libcurl to travis build.
    
    Adjusting request method handling in InvokeHTTP
    
    Adjusting timing for http get integration test
    
    Signed-off-by: Bryan Rosander <brosander@apache.org>

--- libminifi/test/HttpGetIntegrationTest.cpp
+++ libminifi/test/HttpGetIntegrationTest.cpp
@@ -39,7 +39,7 @@
 std::string test_file_location;
 
 void waitToVerifyProcessor() {
-  std::this_thread::sleep_for(std::chrono::seconds(2));
+  std::this_thread::sleep_for(std::chrono::seconds(10));
 }
 
 int main(int argc, char **argv) {
