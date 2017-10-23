c6665288837500ac16bf99757bf026a9f395e270
Caleb Johnson <me@calebj.io>
2017-10-23 13:55:51 +0000

    MINIFICPP-303 Update to RocksDB 5.8.6

--- thirdparty/rocksdb/util/testharness.h
+++ thirdparty/rocksdb/util/testharness.h
@@ -9,6 +9,12 @@
 
 #pragma once
 
+#ifdef OS_AIX
+#include "gtest/gtest.h"
+#else
+#include <gtest/gtest.h>
+#endif
+
 #include <string>
 #include "rocksdb/env.h"
 
@@ -23,6 +29,12 @@ std::string TmpDir(Env* env = Env::Default());
 // runs may be able to vary the seed.
 int RandomSeed();
 
+::testing::AssertionResult AssertStatus(const char* s_expr, const Status& s);
+
+#define ASSERT_OK(s) ASSERT_PRED_FORMAT1(rocksdb::test::AssertStatus, s)
+#define ASSERT_NOK(s) ASSERT_FALSE((s).ok())
+#define EXPECT_OK(s) EXPECT_PRED_FORMAT1(rocksdb::test::AssertStatus, s)
+#define EXPECT_NOK(s) EXPECT_FALSE((s).ok())
 
 }  // namespace test
 }  // namespace rocksdb
