dec7caef7dd348a1fa80d0f4db5d4fc979f785fa
Andy I. Christianson <andy@andyic.org>
2018-01-08 17:13:27 -0500

    MINIFICPP-358 Added TFExtractTopLabels
    This closes #232.
    
    Signed-off-by: Marc Parisi <phrocker@apache.org>

--- libminifi/test/tensorflow-tests/TensorFlowTests.cpp
+++ libminifi/test/tensorflow-tests/TensorFlowTests.cpp
@@ -25,14 +25,14 @@
 #include <processors/GetFile.h>
 #include <processors/LogAttribute.h>
 #include <TFConvertImageToTensor.h>
+#include <TFExtractTopLabels.h>
 #include "TFApplyGraph.h"
-#include "TFConvertImageToTensor.h"
 
 #define CATCH_CONFIG_MAIN
 
 #include "../TestBase.h"
 
-TEST_CASE("TensorFlow: Apply Graph", "[executescriptTensorFlowApplyGraph]") { // NOLINT
+TEST_CASE("TensorFlow: Apply Graph", "[tfApplyGraph]") { // NOLINT
   TestController testController;
 
   LogTestController::getInstance().setTrace<TestPlan>();
@@ -127,7 +127,7 @@ TEST_CASE("TensorFlow: Apply Graph", "[executescriptTensorFlowApplyGraph]") { //
 
   // Read test TensorFlow graph into TFApplyGraph
   plan->runNextProcessor([&get_file, &in_graph_file, &plan](const std::shared_ptr<core::ProcessContext> context,
-                                     const std::shared_ptr<core::ProcessSession> session) {
+                                                            const std::shared_ptr<core::ProcessSession> session) {
     // Intercept the call so that we can add an attr (won't be required when we have UpdateAttribute processor)
     auto flow_file = session->create();
     session->import(in_graph_file, flow_file, false);
@@ -171,7 +171,7 @@ TEST_CASE("TensorFlow: Apply Graph", "[executescriptTensorFlowApplyGraph]") { //
   }
 }
 
-TEST_CASE("TensorFlow: ConvertImageToTensor", "[executescriptTensorFlowConvertImageToTensor]") { // NOLINT
+TEST_CASE("TensorFlow: ConvertImageToTensor", "[tfConvertImageToTensor]") { // NOLINT
   TestController testController;
 
   LogTestController::getInstance().setTrace<TestPlan>();
@@ -266,8 +266,8 @@ TEST_CASE("TensorFlow: ConvertImageToTensor", "[executescriptTensorFlowConvertIm
   // Write test input image
   {
     // 2x2 single-channel 8 bit per channel
-    const uint8_t in_img_raw[2*2] = {0, 0,
-                                     0, 0};
+    const uint8_t in_img_raw[2 * 2] = {0, 0,
+                                       0, 0};
 
     std::ofstream in_file_stream(in_img_file);
     in_file_stream << in_img_raw;
@@ -298,4 +298,109 @@ TEST_CASE("TensorFlow: ConvertImageToTensor", "[executescriptTensorFlowConvertIm
                               10,    // Height
                               1}));  // Channels
   }
+}
+
+TEST_CASE("TensorFlow: Extract Top Labels", "[tfExtractTopLabels]") { // NOLINT
+  TestController testController;
+
+  LogTestController::getInstance().setTrace<TestPlan>();
+  LogTestController::getInstance().setTrace<processors::TFExtractTopLabels>();
+  LogTestController::getInstance().setTrace<processors::GetFile>();
+  LogTestController::getInstance().setTrace<processors::LogAttribute>();
+
+  auto plan = testController.createPlan();
+  auto repo = std::make_shared<TestRepository>();
+
+  // Define directory for input protocol buffers
+  std::string in_dir("/tmp/gt.XXXXXX");
+  REQUIRE(testController.createTempDirectory(&in_dir[0]) != nullptr);
+
+  // Define input labels file
+  std::string in_labels_file(in_dir);
+  in_labels_file.append("/in_labels");
+
+  // Define input tensor protocol buffer file
+  std::string in_tensor_file(in_dir);
+  in_tensor_file.append("/tensor.pb");
+
+  // Build MiNiFi processing graph
+  auto get_file = plan->addProcessor(
+      "GetFile",
+      "Get Input");
+  plan->setProperty(
+      get_file,
+      processors::GetFile::Directory.getName(), in_dir);
+  plan->setProperty(
+      get_file,
+      processors::GetFile::KeepSourceFile.getName(),
+      "false");
+  plan->addProcessor(
+      "LogAttribute",
+      "Log Pre Extract",
+      core::Relationship("success", "description"),
+      true);
+  auto tf_apply = plan->addProcessor(
+      "TFExtractTopLabels",
+      "Extract",
+      core::Relationship("success", "description"),
+      true);
+  plan->addProcessor(
+      "LogAttribute",
+      "Log Post Extract",
+      core::Relationship("success", "description"),
+      true);
+
+  // Build test labels
+  {
+    // Write labels
+    std::ofstream in_file_stream(in_labels_file);
+    in_file_stream << "label_a\nlabel_b\nlabel_c\nlabel_d\nlabel_e\nlabel_f\nlabel_g\nlabel_h\nlabel_i\nlabel_j\n";
+  }
+
+  // Read labels
+  plan->runNextProcessor([&get_file, &in_labels_file, &plan](const std::shared_ptr<core::ProcessContext> context,
+                                                             const std::shared_ptr<core::ProcessSession> session) {
+    // Intercept the call so that we can add an attr (won't be required when we have UpdateAttribute processor)
+    auto flow_file = session->create();
+    session->import(in_labels_file, flow_file, false);
+    flow_file->addAttribute("tf.type", "labels");
+    session->transfer(flow_file, processors::GetFile::Success);
+    session->commit();
+  });
+
+  plan->runNextProcessor();  // Log
+  plan->runNextProcessor();  // Extract (loads labels)
+
+  // Write input tensor
+  {
+    tensorflow::Tensor input(tensorflow::DT_FLOAT, {10});
+    input.flat<float>().data()[0] = 0.000f;
+    input.flat<float>().data()[1] = 0.400f;
+    input.flat<float>().data()[2] = 0.100f;
+    input.flat<float>().data()[3] = 0.005f;
+    input.flat<float>().data()[4] = 1.000f;
+    input.flat<float>().data()[5] = 0.500f;
+    input.flat<float>().data()[6] = 0.200f;
+    input.flat<float>().data()[7] = 0.000f;
+    input.flat<float>().data()[8] = 0.300f;
+    input.flat<float>().data()[9] = 0.000f;
+    tensorflow::TensorProto tensor_proto;
+    input.AsProtoTensorContent(&tensor_proto);
+
+    std::ofstream in_file_stream(in_tensor_file);
+    tensor_proto.SerializeToOstream(&in_file_stream);
+  }
+
+  plan->reset();
+  plan->runNextProcessor();  // GetFile
+  plan->runNextProcessor();  // Log
+  plan->runNextProcessor();  // Extract
+  plan->runNextProcessor();  // Log
+
+  // Verify labels
+  REQUIRE(LogTestController::getInstance().contains("key:tf.top_label_0 value:label_e"));
+  REQUIRE(LogTestController::getInstance().contains("key:tf.top_label_1 value:label_f"));
+  REQUIRE(LogTestController::getInstance().contains("key:tf.top_label_2 value:label_b"));
+  REQUIRE(LogTestController::getInstance().contains("key:tf.top_label_3 value:label_i"));
+  REQUIRE(LogTestController::getInstance().contains("key:tf.top_label_4 value:label_g"));
 }
