845fc9f1960c42fe5656a92e309c2eb64d8c8207
Andrew I. Christianson <andy@andyic.org>
2018-06-14 05:13:23 -0400

    MINIFICPP-533 Fixed signed/unsigned integer comparison mismatch

--- extensions/civetweb/processors/ListenHTTP.h
+++ extensions/civetweb/processors/ListenHTTP.h
@@ -133,8 +133,8 @@ class ListenHTTP : public core::Processor {
     }
     int64_t process(std::shared_ptr<io::BaseStream> stream) {
       out_str_->resize(stream->getSize());
-      auto num_read = stream->readData(reinterpret_cast<uint8_t *>(&(*out_str_)[0]),
-                                       static_cast<int>(stream->getSize()));
+      uint64_t num_read = stream->readData(reinterpret_cast<uint8_t *>(&(*out_str_)[0]),
+                                           static_cast<int>(stream->getSize()));
 
       if (num_read != stream->getSize()) {
         throw std::runtime_error("GraphReadCallback failed to fully read flow file input stream");
