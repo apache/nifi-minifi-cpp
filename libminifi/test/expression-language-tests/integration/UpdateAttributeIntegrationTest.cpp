53e5f0bae81bfd1c3890d8e343d5e862f58df57f
Marc Parisi <phrocker@apache.org>
2019-01-27 07:32:40 -0500

    MINIFICPP-723: Resolve bug in set property, add a regression test, and warning to avoid this this in the future

---
+++ libminifi/test/expression-language-tests/integration/UpdateAttributeIntegrationTest.cpp
@@ -0,0 +1,66 @@
+/**
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <sys/stat.h>
+#undef NDEBUG
+#include <cassert>
+#include <utility>
+#include <chrono>
+#include <fstream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <type_traits>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include "processors/LogAttribute.h"
+#include "../../integration/IntegrationBase.h"
+#include "../../TestBase.h"
+
+class TestHarness : public IntegrationBase {
+ public:
+  TestHarness() {
+  }
+
+  void testSetup() {
+    LogTestController::getInstance().setInfo<minifi::FlowController>();
+    LogTestController::getInstance().setInfo<processors::LogAttribute>();
+  }
+
+  void cleanup() {
+  }
+
+  void runAssertions() {
+    assert(LogTestController::getInstance().contains("key:route_check_attr value:good"));
+  }
+
+ protected:
+};
+
+int main(int argc, char **argv) {
+  std::string key_dir, test_file_location, url;
+  if (argc > 1) {
+    test_file_location = argv[1];
+  }
+
+  TestHarness harness;
+  harness.run(test_file_location);
+
+  return 0;
+}
