ff04bf217b88700c661a2db2a30d60b490da7a57
Marc Parisi <phrocker@apache.org>
2019-04-26 08:28:52 -0400

    MINIFICPP-827: Install jdk8, maven, and require these to be installed. bash beautify while editing scripts too

--- darwin.sh
+++ darwin.sh
@@ -17,114 +17,117 @@
 # under the License.
 
 verify_enable() {
-  feature="$1"
-  feature_status=${!1}
-  if [ "$feature" = "BUSTACHE_ENABLED" ]; then
-    BUSTACHE_MAX="9"
-    ## we should check the xcode version
-    CLANG_VERSION=`clang --version | head -n 1 | awk '{print $4}'`
-    CLANG_MAJOR=`echo $CLANG_VERSION | cut -d. -f1`
-    CLANG_MINOR=`echo $CLANG_VERSION | cut -d. -f2`
-    CLANG_REVISION=`echo $CLANG_VERSION | cut -d. -f3`
-    if [ "$CLANG_MAJOR" -ge "$BUSTACHE_MAX" ]; then
-      echo "false"
+    feature="$1"
+    feature_status=${!1}
+    if [ "$feature" = "BUSTACHE_ENABLED" ]; then
+        BUSTACHE_MAX="9"
+        ## we should check the xcode version
+        CLANG_VERSION=`clang --version | head -n 1 | awk '{print $4}'`
+        CLANG_MAJOR=`echo $CLANG_VERSION | cut -d. -f1`
+        CLANG_MINOR=`echo $CLANG_VERSION | cut -d. -f2`
+        CLANG_REVISION=`echo $CLANG_VERSION | cut -d. -f3`
+        if [ "$CLANG_MAJOR" -ge "$BUSTACHE_MAX" ]; then
+            echo "false"
+        else
+            echo "true"
+        fi
     else
-      echo "true"
+        echo "true"
     fi
-  else
-    echo "true"
-  fi
 }
 
 add_os_flags(){
-  :
+    :
 }
 
 install_bison() {
-  BISON_INSTALLED="false"
-  if [ -x "$(command -v bison)" ]; then
-    BISON_VERSION=`bison --version | head -n 1 | awk '{print $4}'`
-    BISON_MAJOR=`echo $BISON_VERSION | cut -d. -f1`
-    if (( BISON_MAJOR >= 3 )); then
-      BISON_INSTALLED="true"
+    BISON_INSTALLED="false"
+    if [ -x "$(command -v bison)" ]; then
+        BISON_VERSION=`bison --version | head -n 1 | awk '{print $4}'`
+        BISON_MAJOR=`echo $BISON_VERSION | cut -d. -f1`
+        if (( BISON_MAJOR >= 3 )); then
+            BISON_INSTALLED="true"
+        fi
     fi
-  fi
-  if [ "$BISON_INSTALLED" = "false" ]; then
-    wget https://ftp.gnu.org/gnu/bison/bison-3.0.5.tar.xz
-    tar xvf bison-3.0.5.tar.xz
-    pushd bison-3.0.5
-    ./configure
-    make
-    sudo make install
-    popd
-  fi
+    if [ "$BISON_INSTALLED" = "false" ]; then
+        wget https://ftp.gnu.org/gnu/bison/bison-3.0.5.tar.xz
+        tar xvf bison-3.0.5.tar.xz
+        pushd bison-3.0.5
+        ./configure
+        make
+        sudo make install
+        popd
+    fi
 
 }
 
 bootstrap_cmake(){
-  brew install cmake
+    brew install cmake
 }
 build_deps(){
 
-  COMMAND="brew install cmake"
-  INSTALLED=()
-  INSTALLED+=("bzip2")
-  for option in "${OPTIONS[@]}" ; do
-    option_value="${!option}"
-    if [ "$option_value" = "${TRUE}" ]; then
-      # option is enabled
-      FOUND_VALUE=""
-      for cmake_opt in "${DEPENDENCIES[@]}" ; do
-        KEY=${cmake_opt%%:*}
-        VALUE=${cmake_opt#*:}
-        if [ "$KEY" = "$option" ]; then
-          FOUND_VALUE="$VALUE"
-          if [ "$FOUND_VALUE" = "libcurl" ]; then
-            brew install curl
-          elif [ "$FOUND_VALUE" = "libpcap" ]; then
-            INSTALLED+=("libpcap")
-          elif [ "$FOUND_VALUE" = "openssl" ]; then
-            INSTALLED+=("openssl")
-          elif [ "$FOUND_VALUE" = "libusb" ]; then
-            INSTALLED+=("libusb")
-          elif [ "$FOUND_VALUE" = "libpng" ]; then
-            INSTALLED+=("libpng")
-          elif [ "$FOUND_VALUE" = "bison" ]; then
-            install_bison
-          elif [ "$FOUND_VALUE" = "flex" ]; then
-            INSTALLED+=("flex")
-          elif [ "$FOUND_VALUE" = "python" ]; then
-            INSTALLED+=("python")
-          elif [ "$FOUND_VALUE" = "boost" ]; then
-            INSTALLED+=("boost")
-          elif [ "$FOUND_VALUE" = "lua" ]; then
-            INSTALLED+=("lua")
-          elif [ "$FOUND_VALUE" = "libtool" ]; then
-            INSTALLED+=("libtool")
-          elif [ "$FOUND_VALUE" = "automake" ]; then
-            INSTALLED+=("automake")
-            INSTALLED+=("autoconf")
-          elif [ "$FOUND_VALUE" = "gpsd" ]; then
-            INSTALLED+=("gpsd")
-          elif [ "$FOUND_VALUE" = "libarchive" ]; then
-            INSTALLED+=("bzip2")
-          fi
+    COMMAND="brew install cmake"
+    INSTALLED=()
+    INSTALLED+=("bzip2")
+    for option in "${OPTIONS[@]}" ; do
+        option_value="${!option}"
+        if [ "$option_value" = "${TRUE}" ]; then
+            # option is enabled
+            FOUND_VALUE=""
+            for cmake_opt in "${DEPENDENCIES[@]}" ; do
+                KEY=${cmake_opt%%:*}
+                VALUE=${cmake_opt#*:}
+                if [ "$KEY" = "$option" ]; then
+                    FOUND_VALUE="$VALUE"
+                    if [ "$FOUND_VALUE" = "libcurl" ]; then
+                        brew install curl
+                    elif [ "$FOUND_VALUE" = "libpcap" ]; then
+                        INSTALLED+=("libpcap")
+                    elif [ "$FOUND_VALUE" = "openssl" ]; then
+                        INSTALLED+=("openssl")
+                    elif [ "$FOUND_VALUE" = "libusb" ]; then
+                        INSTALLED+=("libusb")
+                    elif [ "$FOUND_VALUE" = "libpng" ]; then
+                        INSTALLED+=("libpng")
+                    elif [ "$FOUND_VALUE" = "bison" ]; then
+                        install_bison
+                    elif [ "$FOUND_VALUE" = "flex" ]; then
+                        INSTALLED+=("flex")
+                    elif [ "$FOUND_VALUE" = "python" ]; then
+                        INSTALLED+=("python")
+                    elif [ "$FOUND_VALUE" = "boost" ]; then
+                        INSTALLED+=("boost")
+                    elif [ "$FOUND_VALUE" = "lua" ]; then
+                        INSTALLED+=("lua")
+                    elif [ "$FOUND_VALUE" = "jnibuild" ]; then
+                        brew cask install java8 > /dev/null 2>&1
+                        INSTALLED+=("maven")
+                    elif [ "$FOUND_VALUE" = "libtool" ]; then
+                        INSTALLED+=("libtool")
+                    elif [ "$FOUND_VALUE" = "automake" ]; then
+                        INSTALLED+=("automake")
+                        INSTALLED+=("autoconf")
+                    elif [ "$FOUND_VALUE" = "gpsd" ]; then
+                        INSTALLED+=("gpsd")
+                    elif [ "$FOUND_VALUE" = "libarchive" ]; then
+                        INSTALLED+=("bzip2")
+                    fi
+                fi
+            done
+
         fi
-      done
+    done
 
-    fi
-  done
+    for option in "${INSTALLED[@]}" ; do
+        COMMAND="${COMMAND} $option"
+    done
 
-  for option in "${INSTALLED[@]}" ; do
-    COMMAND="${COMMAND} $option"
-  done
+    echo "Ensuring you have all dependencies installed..."
+    ${COMMAND} > /dev/null 2>&1
 
-  echo "Ensuring you have all dependencies installed..."
-  ${COMMAND} > /dev/null 2>&1
-
-  for option in "${INSTALLED[@]}" ; do
-    if [ "$option" = "curl" ]; then
-      brew link curl --force > /dev/null 2>&1
-    fi
-  done
+    for option in "${INSTALLED[@]}" ; do
+        if [ "$option" = "curl" ]; then
+            brew link curl --force > /dev/null 2>&1
+        fi
+    done
 }
--- fedora.sh
+++ fedora.sh
@@ -17,80 +17,83 @@
 # under the License.
 
 verify_enable(){
-  feature="$1"
-  feature_status=${!1}
-  verify_gcc_enable $feature
+    feature="$1"
+    feature_status=${!1}
+    verify_gcc_enable $feature
 }
 add_os_flags() {
-  :
+    :
 }
 bootstrap_cmake(){
-  sudo yum -y install wget
-  wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-  sudo yum -y install epel-release-latest-7.noarch.rpm
-  sudo yum -y install cmake3
+    sudo yum -y install wget
+    wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+    sudo yum -y install epel-release-latest-7.noarch.rpm
+    sudo yum -y install cmake3
 }
 build_deps(){
-# Install epel-release so that cmake3 will be available for installation
-  sudo yum -y install wget
-  wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-  sudo yum -y install epel-release-latest-7.noarch.rpm
+    # Install epel-release so that cmake3 will be available for installation
+    sudo yum -y install wget
+    wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+    sudo yum -y install epel-release-latest-7.noarch.rpm
 
-  COMMAND="sudo yum -y install gcc gcc-c++ libuuid libuuid-devel patch"
-  INSTALLED=()
-  INSTALLED+=("bzip2-devel")
-  for option in "${OPTIONS[@]}" ; do
-    option_value="${!option}"
-    if [ "$option_value" = "${TRUE}" ]; then
-      # option is enabled
-      FOUND_VALUE=""
-      for cmake_opt in "${DEPENDENCIES[@]}" ; do
-        KEY=${cmake_opt%%:*}
-        VALUE=${cmake_opt#*:}
-        if [ "$KEY" = "$option" ]; then
-          FOUND_VALUE="$VALUE"
-          echo $FOUND_VALUE
-          if [ "$FOUND_VALUE" = "libcurl" ]; then
-            INSTALLED+=("libcurl-devel")
-          elif [ "$FOUND_VALUE" = "libpcap" ]; then
-            INSTALLED+=("libpcap-devel")
-          elif [ "$FOUND_VALUE" = "openssl" ]; then
-            INSTALLED+=("openssl")
-            INSTALLED+=("openssl-devel")
-          elif [ "$FOUND_VALUE" = "libusb" ]; then
-            INSTALLED+=("libusb-devel")
-          elif [ "$FOUND_VALUE" = "libpng" ]; then
-            INSTALLED+=("libpng-devel")
-          elif [ "$FOUND_VALUE" = "bison" ]; then
-            INSTALLED+=("bison")
-          elif [ "$FOUND_VALUE" = "flex" ]; then
-            INSTALLED+=("flex")
-	    echo "add flex"
-          elif [ "$FOUND_VALUE" = "python" ]; then
-            INSTALLED+=("python3-devel")
-          elif [ "$FOUND_VALUE" = "lua" ]; then
-            INSTALLED+=("lua-devel")
-	  elif [ "$FOUND_VALUE" = "libtool" ]; then
-            INSTALLED+=("libtool")
-          elif [ "$FOUND_VALUE" = "autoconf" ]; then
-            INSTALLED+=("autoconf")
-          elif [ "$FOUND_VALUE" = "automake" ]; then
-	    INSTALLED+=("automake")
-          elif [ "$FOUND_VALUE" = "gpsd" ]; then
-            INSTALLED+=("gpsd-devel")
-          elif [ "$FOUND_VALUE" = "libarchive" ]; then
-            INSTALLED+=("xz-devel")
-          fi
+    COMMAND="sudo yum -y install gcc gcc-c++ libuuid libuuid-devel patch"
+    INSTALLED=()
+    INSTALLED+=("bzip2-devel")
+    for option in "${OPTIONS[@]}" ; do
+        option_value="${!option}"
+        if [ "$option_value" = "${TRUE}" ]; then
+            # option is enabled
+            FOUND_VALUE=""
+            for cmake_opt in "${DEPENDENCIES[@]}" ; do
+                KEY=${cmake_opt%%:*}
+                VALUE=${cmake_opt#*:}
+                if [ "$KEY" = "$option" ]; then
+                    FOUND_VALUE="$VALUE"
+                    echo $FOUND_VALUE
+                    if [ "$FOUND_VALUE" = "libcurl" ]; then
+                        INSTALLED+=("libcurl-devel")
+                    elif [ "$FOUND_VALUE" = "libpcap" ]; then
+                        INSTALLED+=("libpcap-devel")
+                    elif [ "$FOUND_VALUE" = "openssl" ]; then
+                        INSTALLED+=("openssl")
+                        INSTALLED+=("openssl-devel")
+                    elif [ "$FOUND_VALUE" = "libusb" ]; then
+                        INSTALLED+=("libusb-devel")
+                    elif [ "$FOUND_VALUE" = "libpng" ]; then
+                        INSTALLED+=("libpng-devel")
+                    elif [ "$FOUND_VALUE" = "bison" ]; then
+                        INSTALLED+=("bison")
+                    elif [ "$FOUND_VALUE" = "flex" ]; then
+                        INSTALLED+=("flex")
+                    elif [ "$FOUND_VALUE" = "jnibuild" ]; then
+                        INSTALLED+=("java-1.8.0-openjdk")
+                        INSTALLED+=("java-1.8.0-openjdk-devel")
+                        INSTALLED+=("maven")
+                    elif [ "$FOUND_VALUE" = "python" ]; then
+                        INSTALLED+=("python3-devel")
+                    elif [ "$FOUND_VALUE" = "lua" ]; then
+                        INSTALLED+=("lua-devel")
+                    elif [ "$FOUND_VALUE" = "libtool" ]; then
+                        INSTALLED+=("libtool")
+                    elif [ "$FOUND_VALUE" = "autoconf" ]; then
+                        INSTALLED+=("autoconf")
+                    elif [ "$FOUND_VALUE" = "automake" ]; then
+                        INSTALLED+=("automake")
+                    elif [ "$FOUND_VALUE" = "gpsd" ]; then
+                        INSTALLED+=("gpsd-devel")
+                    elif [ "$FOUND_VALUE" = "libarchive" ]; then
+                        INSTALLED+=("xz-devel")
+                    fi
+                fi
+            done
+
         fi
-      done
+    done
 
-    fi
-  done
-
-  for option in "${INSTALLED[@]}" ; do
-    COMMAND="${COMMAND} $option"
-  done
-echo "Installing ${COMMAND}"
-  ${COMMAND}
+    for option in "${INSTALLED[@]}" ; do
+        COMMAND="${COMMAND} $option"
+    done
+    echo "Installing ${COMMAND}"
+    ${COMMAND}
 
 }
