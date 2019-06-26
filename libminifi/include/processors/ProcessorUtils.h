370ce0d7baa644a390e837f20c7ed7488f194de8
Marc Parisi <phrocker@apache.org>
2019-06-26 19:00:02 -0400

    MINIFICPP-936: Add canonical name check back into processor utils

--- libminifi/include/processors/ProcessorUtils.h
+++ libminifi/include/processors/ProcessorUtils.h
@@ -22,20 +22,31 @@ class ProcessorUtils {
   static inline std::shared_ptr<core::Processor> createProcessor(const std::string &class_short_name, const std::string &canonicalName, utils::Identifier &uuid,
                                                                  const std::shared_ptr<minifi::io::StreamFactory> &stream_factory) {
     auto ptr = core::ClassLoader::getDefaultClassLoader().instantiate(class_short_name, uuid);
+    // fallback to the canonical name if the short name does not work, then attempt JNI bindings if they exist
     if (ptr == nullptr) {
-      ptr = core::ClassLoader::getDefaultClassLoader().instantiate("ExecuteJavaClass", uuid);
-      if (ptr != nullptr) {
-        std::shared_ptr<core::Processor> processor = std::static_pointer_cast<core::Processor>(ptr);
-        processor->initialize();
-        processor->setProperty("NiFi Processor", canonicalName);
-        processor->setStreamFactory(stream_factory);
-        return processor;
+      ptr = core::ClassLoader::getDefaultClassLoader().instantiate(canonicalName, uuid);
+      if (ptr == nullptr) {
+        ptr = core::ClassLoader::getDefaultClassLoader().instantiate("ExecuteJavaClass", uuid);
+        if (ptr != nullptr) {
+          std::shared_ptr<core::Processor> processor = std::dynamic_pointer_cast<core::Processor>(ptr);
+          if (processor == nullptr) {
+            throw std::runtime_error("Invalid return from the classloader");
+          }
+          processor->initialize();
+          processor->setProperty("NiFi Processor", canonicalName);
+          processor->setStreamFactory(stream_factory);
+          return processor;
+        }
       }
     }
-    if (nullptr == ptr) {
+    if (ptr == nullptr) {
       return nullptr;
     }
-    auto returnPtr = std::static_pointer_cast<core::Processor>(ptr);
+    auto returnPtr = std::dynamic_pointer_cast<core::Processor>(ptr);
+
+    if (returnPtr == nullptr) {
+      throw std::runtime_error("Invalid return from the classloader");
+    }
 
     returnPtr->initialize();
 
