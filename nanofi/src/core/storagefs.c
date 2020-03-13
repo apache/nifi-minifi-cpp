e947d69ec7ac34e21f71628b076573d99e263203
Murtuza <msharee9@gmail.com>
2020-03-13 10:21:34 -0400

    MINIFICPP-1007 fix

--- nanofi/src/core/storagefs.c
+++ nanofi/src/core/storagefs.c
@@ -21,56 +21,58 @@
 #include <core/storagefs.h>
 
 storage_t * create_storage(const char * root_path) {
-    if (!root_path) return NULL;
+  if (!root_path) return NULL;
 
-    storage_t * strg = (storage_t *)malloc(sizeof(storage_t));
-    memset(strg, 0, sizeof(storage_t));
-    CIDGenerator gen;
-    gen.implementation_ = CUUID_DEFAULT_IMPL;
-    generate_uuid(&gen, strg->uuid);
-    strg->fs_path = concat_path(root_path, strg->uuid);
-    if (make_dir(strg->fs_path) < 0) {
-        return NULL;
-    }
-    return strg;
+  storage_t * strg = (storage_t *)malloc(sizeof(storage_t));
+  memset(strg, 0, sizeof(storage_t));
+  CIDGenerator gen;
+  gen.implementation_ = CUUID_DEFAULT_IMPL;
+  generate_uuid(&gen, strg->uuid);
+  strg->fs_path = concat_path(root_path, strg->uuid);
+  if (make_dir(strg->fs_path) < 0) {
+    free(strg->fs_path);
+    free(strg);
+    return NULL;
+  }
+  return strg;
 }
 
 chunks_t * add_chunk(storage_t * strg, const char * buffer, size_t len) {
-    if (!strg || !buffer || len == 0) return NULL;
+  if (!strg || !buffer || len == 0) return NULL;
 
-    chunks_t * ck = (chunks_t *)malloc(sizeof(chunks_t));
-    strg->last_chunk_number++;
-    ck->buffer = buffer;
-    ck->len = len;
-    snprintf(ck->path, sizeof(ck->path), "_%u", strg->last_chunk_number);
-    LL_APPEND(strg->ct, ck);
-    write_chunk_to_storage(strg, ck);
-    return ck;
+  chunks_t * ck = (chunks_t *)malloc(sizeof(chunks_t));
+  strg->last_chunk_number++;
+  ck->buffer = buffer;
+  ck->len = len;
+  snprintf(ck->path, sizeof(ck->path), "_%u", strg->last_chunk_number);
+  LL_APPEND(strg->ct, ck);
+  write_chunk_to_storage(strg, ck);
+  return ck;
 }
 
 int write_chunk_to_storage(storage_t * strg, chunks_t * ck) {
-    if (!strg || !strg->fs_path || strlen(strg->fs_path) == 0
-            || !ck || strlen(ck->path) == 0)
-        return -1;
+  if (!strg || !strg->fs_path || strlen(strg->fs_path) == 0
+        || !ck || strlen(ck->path) == 0)
+    return -1;
 
-    if (!ck->buffer || ck->len == 0) return -1;
+  if (!ck->buffer || ck->len == 0) return -1;
 
-    const char * path = concat_path(strg->fs_path, ck->path);
-    FILE * fp = fopen(path, "w");
-    if (!fp) {
-        free(path);
-        fclose(fp);
-        return -1;
-    }
+  const char * path = concat_path(strg->fs_path, ck->path);
+  FILE * fp = fopen(path, "w");
+  if (!fp) {
+      free(path);
+      fclose(fp);
+      return -1;
+  }
 
-    if (fwrite(ck->buffer, 1, ck->len, fp) < ck->len) {
-        free(path);
-        fclose(fp);
-        return -1;
-    }
-    free(path);
-    fclose(fp);
-    return 0;
+  if (fwrite(ck->buffer, 1, ck->len, fp) < ck->len) {
+      free(path);
+      fclose(fp);
+      return -1;
+  }
+  free(path);
+  fclose(fp);
+  return 0;
 }
 
 void add_chunk_attributes(chunks_t * chunk, attribute_set as) {
