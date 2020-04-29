10469dd95d9a93505ef84627b33c67ab19b157a1
Adam Hunyadi <hunyadi.dev@gmail.com>
2020-04-29 12:35:18 +0200

    MINIFICPP-1202 - Add comment recommended by szaszm to explain TMP on MinifiConcurrentQueue

--- libminifi/include/utils/MinifiConcurrentQueue.h
+++ libminifi/include/utils/MinifiConcurrentQueue.h
@@ -44,9 +44,20 @@ struct TryMoveCall {
     static void call(Fun&& fun, T& elem) { std::forward<Fun>(fun)(elem); }
 };
 
-// 1. std::declval<T>() resolves to a non-evaluated rvalue of T
-// 2. std::declval<FunType>()(*1*) resolves to a non-evaluated function object with T& argument
-// 4. void_t<decltype(*2*)> whenever the expression is well formed, the template is enabled and cosidered more specifialized than the default
+// 1.) std::declval looks similar to this: template<typename T> T&& declval();.
+//     Not defined, therefore it's only usable in unevaluated context.
+//     No requirements regarding T, therefore makes it possible to create hypothetical objects
+//     without requiring e.g. a default constructor and a destructor, like the T{} expression does.
+// 2.) std::declval<FunType>() resolves to an object of type FunType. If FunType is an lvalue reference,
+//     then this will also result in an lvalue reference due to reference collapsing.
+// 3.) std::declval<FunType>()(std::declval<T>()) resolves to an object of the result type of
+//     a call on a function object of type FunType with an rvalue argument of type T.
+//     It is ill-formed if the function object expect an lvalue reference.
+//         - Example: FunType is a pointer to a bool(int) and T is int. This expression will result in a bool object.
+//         - Example: FunType is a function object modeling bool(int&) and T is int. This expression will be ill-formed because it's illegal to bind an int rvalue to an int&.
+// 4.) void_t<decltype(*3*)> checks for the well-formedness of 3., then discards it.
+//     If 3. is ill-formed, then this specialization is ignored through SFINAE.
+//     If well-formed, then it's considered more specialized than the other and takes precedence.
 template<typename FunType, typename T>
 struct TryMoveCall<FunType, T, void_t<decltype(std::declval<FunType>()(std::declval<T>()))>> {
     template<typename Fun>
