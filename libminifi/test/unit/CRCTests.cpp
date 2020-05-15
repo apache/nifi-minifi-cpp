445a5d92cde92935374de291c2479fd7ac5e6dcb
Ferenc Gerlits <fgerlits@gmail.com>
2020-05-15 15:33:10 +0200

    MINIFICPP-1177 Add c'tor with initial CRC value
    Also implemented some API methods where the inherited version was incorrect.

--- libminifi/test/unit/CRCTests.cpp
+++ libminifi/test/unit/CRCTests.cpp
@@ -61,3 +61,40 @@ TEST_CASE("Test CRC5", "[testcrc5]") {
   test.write(number);
   REQUIRE(3753740124 == test.getCRC());
 }
+
+TEST_CASE("CRCStream with initial crc = 0 is the same as without initial crc", "[initial_crc_arg]") {
+  org::apache::nifi::minifi::io::BaseStream base1;
+  org::apache::nifi::minifi::io::CRCStream<org::apache::nifi::minifi::io::BaseStream> test_noinit(&base1);
+
+  org::apache::nifi::minifi::io::BaseStream base2;
+  org::apache::nifi::minifi::io::CRCStream<org::apache::nifi::minifi::io::BaseStream> test_initzero(&base2, 0);
+
+  const std::string textString = "The quick brown fox jumps over the lazy dog";
+  std::vector<uint8_t> textVector1(textString.begin(), textString.end());
+  std::vector<uint8_t> textVector2(textString.begin(), textString.end());
+
+  test_noinit.writeData(textVector1, textVector1.size());
+  test_initzero.writeData(textVector2, textVector2.size());
+  REQUIRE(test_noinit.getCRC() == test_initzero.getCRC());
+}
+
+TEST_CASE("CRCStream: one long write is the same as writing in two pieces", "[initial_crc_arg]") {
+  const std::string textString = "The quick brown fox jumps over the lazy dog";
+
+  org::apache::nifi::minifi::io::BaseStream base_full;
+  org::apache::nifi::minifi::io::CRCStream<org::apache::nifi::minifi::io::BaseStream> test_full(&base_full, 0);
+  std::vector<uint8_t> textVector_full(textString.begin(), textString.end());
+  test_full.writeData(textVector_full, textVector_full.size());
+
+  org::apache::nifi::minifi::io::BaseStream base_piece1;
+  org::apache::nifi::minifi::io::CRCStream<org::apache::nifi::minifi::io::BaseStream> test_piece1(&base_piece1, 0);
+  std::vector<uint8_t> textVector_piece1(textString.begin(), textString.begin() + 15);
+  test_piece1.writeData(textVector_piece1, textVector_piece1.size());
+
+  org::apache::nifi::minifi::io::BaseStream base_piece2;
+  org::apache::nifi::minifi::io::CRCStream<org::apache::nifi::minifi::io::BaseStream> test_piece2(&base_piece2, test_piece1.getCRC());
+  std::vector<uint8_t> textVector_piece2(textString.begin() + 15, textString.end());
+  test_piece2.writeData(textVector_piece2, textVector_piece2.size());
+
+  REQUIRE(test_full.getCRC() == test_piece2.getCRC());
+}
