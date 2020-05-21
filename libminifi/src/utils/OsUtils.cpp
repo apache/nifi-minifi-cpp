00850d92a93781e2753d2c52a07376dfd210f98f
Adam Hunyadi <hunyadi.dev@gmail.com>
2020-05-21 11:02:14 +0200

    MINIFICPP-1203 - Swap include order in OSUtils.cpp for correct Windows-specific symbol imports

--- libminifi/src/utils/OsUtils.cpp
+++ libminifi/src/utils/OsUtils.cpp
@@ -27,8 +27,8 @@
 #ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
 #endif
-#include <sddl.h>
 #include <Windows.h>
+#include <sddl.h>
 #pragma comment(lib, "Ws2_32.lib")
 #else
 #include <arpa/inet.h>
