35bca551e1949e62f234de2dcc588f6998ec4d85
Adam Debreceni <adam.debreceni@protonmail.com>
2020-09-03 08:50:33 +0200

    MINIFICPP-1340 - Add virtual destructor

--- libminifi/include/core/ContentSession.h
+++ libminifi/include/core/ContentSession.h
@@ -50,6 +50,8 @@ class ContentSession {
 
   void rollback();
 
+  virtual ~ContentSession() = default;
+
  protected:
   std::map<std::shared_ptr<ResourceClaim>, std::shared_ptr<io::BaseStream>> managedResources_;
   std::map<std::shared_ptr<ResourceClaim>, std::shared_ptr<io::BaseStream>> extendedResources_;
