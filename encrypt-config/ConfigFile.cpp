e4b312f915c3572cc17c02411b932fb2845173a5
Ferenc Gerlits <fgerlits@gmail.com>
2020-09-03 18:13:48 +0200

    MINIFICPP-1323 Add some consts and a move

--- encrypt-config/ConfigFile.cpp
+++ encrypt-config/ConfigFile.cpp
@@ -160,14 +160,14 @@ int ConfigFile::encryptSensitiveProperties(const utils::crypto::Bytes& encryptio
 
 std::vector<std::string> ConfigFile::getSensitiveProperties() const {
   std::vector<std::string> sensitive_properties(DEFAULT_SENSITIVE_PROPERTIES.begin(), DEFAULT_SENSITIVE_PROPERTIES.end());
-  utils::optional<std::string> additional_sensitive_props_list = getValue(ADDITIONAL_SENSITIVE_PROPS_PROPERTY_NAME);
+  const utils::optional<std::string> additional_sensitive_props_list = getValue(ADDITIONAL_SENSITIVE_PROPS_PROPERTY_NAME);
   if (additional_sensitive_props_list) {
     std::vector<std::string> additional_sensitive_properties = utils::StringUtils::split(*additional_sensitive_props_list, ",");
     sensitive_properties = mergeProperties(sensitive_properties, additional_sensitive_properties);
   }
 
-  auto not_found = [this](const std::string& property_name) { return !getValue(property_name); };
-  auto new_end = std::remove_if(sensitive_properties.begin(), sensitive_properties.end(), not_found);
+  const auto not_found = [this](const std::string& property_name) { return !getValue(property_name); };
+  const auto new_end = std::remove_if(sensitive_properties.begin(), sensitive_properties.end(), not_found);
   sensitive_properties.erase(new_end, sensitive_properties.end());
 
   return sensitive_properties;
@@ -178,7 +178,7 @@ std::vector<std::string> ConfigFile::mergeProperties(std::vector<std::string> pr
   for (const auto& property_name : additional_properties) {
     std::string property_name_trimmed = utils::StringUtils::trim(property_name);
     if (!property_name_trimmed.empty()) {
-      properties.push_back(property_name_trimmed);
+      properties.push_back(std::move(property_name_trimmed));
     }
   }
 
