43852d0a787bc7396e7fa2ac97a1e5dc2625de63
Adam Hunyadi <hunyadi.dev@gmail.com>
2020-09-09 15:51:36 +0200

    MINIFICPP-1288 - Refactor YamlConfiguration::parsePropertiesNodeYaml

--- libminifi/include/core/yaml/YamlConfiguration.h
+++ libminifi/include/core/yaml/YamlConfiguration.h
@@ -266,6 +266,10 @@ class YamlConfiguration : public FlowConfiguration {
    */
   void parseProvenanceReportingYaml(YAML::Node *reportNode, core::ProcessGroup *parentGroup);
 
+  PropertyValue getValidatedProcessorPropertyForDefaultTypeInfo(const core::Property& propertyFromProcessor, const YAML::Node& propertyValueNode);
+  void parsePropertyValueSequence(const std::string& propertyName, const YAML::Node& propertyValueNode, std::shared_ptr<core::ConfigurableComponent> processor, const std::string &yaml_section);
+  void parseSingleProperty(const std::string& propertyName, const YAML::Node& propertyValueNode, std::shared_ptr<core::ConfigurableComponent> processor);
+  void parsePropertyNodeElement(const std::string& propertyName, const YAML::Node& propertyValueNode, std::shared_ptr<core::ConfigurableComponent> processor, const std::string &yaml_section);
   /**
    * A helper function to parse the Properties Node YAML for a processor.
    *
