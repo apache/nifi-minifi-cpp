9e282acd944a71f12a80d40918a01393a7c916ff
Adam Debreceni <adam.debreceni@protonmail.com>
2020-11-30 08:54:24 +0100

    MINIFICPP-1412 - Factor out logic

--- libminifi/src/core/ProcessSession.cpp
+++ libminifi/src/core/ProcessSession.cpp
@@ -744,18 +744,7 @@ void ProcessSession::commit() {
         }
     }
 
-    for (auto& cq : connectionQueues) {
-      for (auto& flowFile : cq.second) {
-        auto claim = flowFile->getResourceClaim();
-        if (!claim) {
-          logger_->log_debug("Processor %s (%s) did not create a ResourceClaim, creating an empty one",
-                             process_context_->getProcessorNode()->getUUIDStr(),
-                             process_context_->getProcessorNode()->getName());
-          OutputStreamPipe emptyStreamCallback(std::make_shared<io::BufferStream>());
-          write(flowFile, &emptyStreamCallback);
-        }
-      }
-    }
+    ensureNonNullResourceClaim(connectionQueues);
 
     content_session_->commit();
 
@@ -900,6 +889,22 @@ void ProcessSession::persistFlowFilesBeforeTransfer(
       if (originalClaim) originalClaim->decreaseFlowFileRecordOwnedCount();
 
       ff->setStoredToRepository(true);
+    }
+  }
+}
+
+void ProcessSession::ensureNonNullResourceClaim(
+    const std::map<std::shared_ptr<Connectable>, std::vector<std::shared_ptr<core::FlowFile>>> &transactionMap) {
+  for (auto& transaction : transactionMap) {
+    for (auto& flowFile : transaction.second) {
+      auto claim = flowFile->getResourceClaim();
+      if (!claim) {
+        logger_->log_debug("Processor %s (%s) did not create a ResourceClaim, creating an empty one",
+                           process_context_->getProcessorNode()->getUUIDStr(),
+                           process_context_->getProcessorNode()->getName());
+        OutputStreamPipe emptyStreamCallback(std::make_shared<io::BufferStream>());
+        write(flowFile, &emptyStreamCallback);
+      }
     }
   }
 }
