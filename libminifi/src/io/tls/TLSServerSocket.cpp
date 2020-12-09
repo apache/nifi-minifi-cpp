670a803a536a107026456628fa7d35dd2c6b7965
Gabor Gyimesi <gamezbird@gmail.com>
2020-12-09 18:23:59 +0100

    Stabilize SecureSocketGetTCP tests
    The flakiness of the test was caused by resetting the server socket
    before the client had the chance of connecting. We need to wait for the
    client connection to be established before terminating the server, also
    lowering the timeout between socket accepts keeps the test's runtime
    short.

--- libminifi/src/io/tls/TLSServerSocket.cpp
+++ libminifi/src/io/tls/TLSServerSocket.cpp
@@ -87,13 +87,13 @@ void TLSServerSocket::registerCallback(std::function<bool()> accept_function, st
  * Initializes the socket
  * @return result of the creation operation.
  */
-void TLSServerSocket::registerCallback(std::function<bool()> accept_function, std::function<int(std::vector<uint8_t>*, int *)> handler) {
-  fx = [this](std::function<bool()> accept_function, std::function<int(std::vector<uint8_t>*, int *)> handler) {
+void TLSServerSocket::registerCallback(std::function<bool()> accept_function, std::function<int(std::vector<uint8_t>*, int *)> handler, uint16_t timeout) {
+  fx = [this](std::function<bool()> accept_function, std::function<int(std::vector<uint8_t>*, int *)> handler, uint16_t timeout) {
     int ret = 0;
     std::vector<int> fds;
     int size;
     while (accept_function()) {
-      int fd = select_descriptor(3000);
+      int fd = select_descriptor(timeout);
       if (fd > 0) {
         int fd_remove = 0;
         std::vector<uint8_t> data;
@@ -127,7 +127,7 @@ void TLSServerSocket::registerCallback(std::function<bool()> accept_function, st
       close_ssl(fd);
     }
   };
-  server_read_thread_ = std::thread(fx, accept_function, handler);
+  server_read_thread_ = std::thread(fx, accept_function, handler, timeout);
 }
 
 void TLSServerSocket::close_fd(int fd) {
