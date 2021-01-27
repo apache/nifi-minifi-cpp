4492021879c2151b7e82e3b4624a9c0d890a5607
Marton Szasz <szaszm01@gmail.com>
2021-01-27 17:26:22 +0100

    fix socket() call, add error logging

--- extensions/standard-processors/tests/integration/TLSServerSocketSupportedProtocolsTest.cpp
+++ extensions/standard-processors/tests/integration/TLSServerSocketSupportedProtocolsTest.cpp
@@ -19,6 +19,8 @@
 #include <sys/stat.h>
 #include <chrono>
 #include <thread>
+#include <cerrno>
+#include <cinttypes>
 #undef NDEBUG
 #include <cassert>
 #include <utility>
@@ -39,13 +41,74 @@ using SocketDescriptor = int;
 static constexpr SocketDescriptor INVALID_SOCKET = -1;
 #endif /* WIN32 */
 
+namespace {
+const char* str_family(int family) {
+  switch(family) {
+    case AF_INET: return "AF_INET";
+    case AF_INET6: return "AF_INET6";
+    default: return "(n/a)";
+  }
+}
+
+const char* str_socktype(int socktype) {
+  switch(socktype) {
+    case SOCK_STREAM: return "SOCK_STREAM";
+    case SOCK_DGRAM: return "SOCK_DGRAM";
+    default: return "(n/a)";
+  }
+}
+
+const char* str_proto(int protocol) {
+  switch(protocol) {
+    case IPPROTO_TCP: return "IPPROTO_TCP";
+    case IPPROTO_UDP: return "IPPROTO_UDP";
+    case IPPROTO_IP: return "IPPROTO_IP";
+    case IPPROTO_ICMP: return "IPPROTO_ICMP";
+    default: return "(n/a)";
+  }
+}
+
+std::string str_addr(const sockaddr* const sa) {
+  char buf[128] = {0};
+  switch (sa->sa_family) {
+    case AF_INET: {
+      sockaddr_in sin{};
+      memcpy(&sin, sa, sizeof(sockaddr_in));
+      const auto addr_str = inet_ntop(AF_INET, &sin.sin_addr, buf, sizeof(buf));
+      if (!addr_str) {
+        throw std::runtime_error{minifi::io::get_last_socket_error_message()};
+      }
+      return std::string{addr_str};
+    }
+    case AF_INET6: {
+      sockaddr_in6 sin6{};
+      memcpy(&sin6, sa, sizeof(sockaddr_in6));
+      const auto addr_str = inet_ntop(AF_INET6, &sin6.sin6_addr, buf, sizeof(buf));
+      if (!addr_str) {
+        throw std::runtime_error{minifi::io::get_last_socket_error_message()};
+      }
+      return std::string{addr_str};
+    }
+    default: return "(n/a)";
+  }
+}
+
+void log_addrinfo(addrinfo* const ai, logging::Logger& logger) {
+  logger.log_debug(".ai_family: %d %s\n", ai->ai_family, str_family(ai->ai_family));
+  logger.log_debug(".ai_socktype: %d %s\n", ai->ai_socktype, str_socktype(ai->ai_socktype));
+  logger.log_debug(".ai_protocol: %d %s\n", ai->ai_protocol, str_proto(ai->ai_protocol));
+  logger.log_debug(".ai_addr: %s\n", str_addr(ai->ai_addr).c_str());
+  logger.log_debug(".ai_addrlen: %" PRIu32 "\n", ai->ai_addrlen);
+}
+}  // namespace
+
 class SimpleSSLTestClient  {
  public:
   SimpleSSLTestClient(const SSL_METHOD* method, const std::string& host, const std::string& port) :
     host_(host),
     port_(port) {
       ctx_ = SSL_CTX_new(method);
-      sfd_ = openConnection(host_.c_str(), port_.c_str());
+      sfd_ = openConnection(host_.c_str(), port_.c_str(), *logger_);
       if (ctx_ != nullptr)
         ssl_ = SSL_new(ctx_);
       if (ssl_ != nullptr)
@@ -74,8 +137,9 @@ class SimpleSSLTestClient  {
   SocketDescriptor sfd_;
   std::string host_;
   std::string port_;
+  gsl::not_null<std::shared_ptr<logging::Logger>> logger_{gsl::make_not_null(logging::LoggerFactory<SimpleSSLTestClient>::getLogger())};
 
-  static SocketDescriptor openConnection(const char *host_name, const char *port) {
+  static SocketDescriptor openConnection(const char *host_name, const char *port, logging::Logger& logger) {
     struct hostent *host;
     assert((host = gethostbyname(host_name)) != nullptr);
     struct addrinfo hints = {0}, *addrs;
@@ -86,12 +150,17 @@ class SimpleSSLTestClient  {
     assert(status == 0);
     SocketDescriptor sfd = INVALID_SOCKET;
     for (struct addrinfo *addr = addrs; addr != nullptr; addr = addr->ai_next) {
-      sfd = socket(addrs->ai_family, addrs->ai_socktype, addrs->ai_protocol);
+      log_addrinfo(addr, logger);
+      sfd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
       if (sfd == INVALID_SOCKET) {
-          continue;
+        logger.log_info("socket: %s\n", minifi::io::get_last_socket_error_message());
+        continue;
       }
-      if (connect(sfd, addr->ai_addr, addr->ai_addrlen) == 0) {
-          break;
+      const auto connect_result = connect(sfd, addr->ai_addr, addr->ai_addrlen);
+      if (connect_result == 0) {
+        break;
+      } else {
+        logger.log_info("connect to %s: %s\n", str_addr(addr->ai_addr), minifi::io::get_last_socket_error_message());
       }
       sfd = INVALID_SOCKET;
 #ifdef WIN32
