0d2906adba5c9014ec93760d75c96c4489384454
Martin Zink <martin.zink@protonmail.com>
2021-02-10 09:21:45 +0100

    MINIFICPP-1451: ThreadPoolAdjust test transiently failed on Windows CI

--- extensions/http-curl/tests/ThreadPoolAdjust.cpp
+++ extensions/http-curl/tests/ThreadPoolAdjust.cpp
@@ -31,9 +31,11 @@
 #include "processors/LogAttribute.h"
 #include "utils/IntegrationTestUtils.h"
 
+constexpr uint64_t INCREASED_WAITTIME_MSECS = DEFAULT_WAITTIME_MSECS * 1.5;
+
 class HttpTestHarness : public IntegrationBase {
  public:
-  HttpTestHarness() {
+  HttpTestHarness() : IntegrationBase(INCREASED_WAITTIME_MSECS) {
     char format[] = "/tmp/ssth.XXXXXX";
     dir = testController.createTempDirectory(format);
   }
