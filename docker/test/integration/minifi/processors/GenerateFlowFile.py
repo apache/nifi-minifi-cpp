f66fd84df4207bcaf86daa1568f04d79f47d7178
Gabor Gyimesi <gamezbird@gmail.com>
2021-03-10 12:39:52 +0100

    Fix indentation flake8 warnings in processors

--- docker/test/integration/minifi/processors/GenerateFlowFile.py
+++ docker/test/integration/minifi/processors/GenerateFlowFile.py
@@ -1,7 +1,9 @@
 from ..core.Processor import Processor
 
+
 class GenerateFlowFile(Processor):
     def __init__(self, schedule={'scheduling period': '2 sec'}):
-        super(GenerateFlowFile, self).__init__('GenerateFlowFile',
-			schedule=schedule,
-			auto_terminate=['success'])
+        super(GenerateFlowFile, self).__init__(
+            'GenerateFlowFile',
+            schedule=schedule,
+            auto_terminate=['success'])
