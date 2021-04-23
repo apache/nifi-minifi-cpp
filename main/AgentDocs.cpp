895863d2f43e24eb5d2ec4343d8cae8aa8e7df38
Adam Hunyadi <hunyadi.dev@gmail.com>
2021-04-23 16:40:01 +0200

    MINIFICPP-1203 - Add missing spaces around << according to linter recommendations
    Script used:
    cat linter_errors.log | grep "Missing spaces around << " | tr ":" " " |
    cut -d" " -f1,2 | sort -rn -k1 -k2 | xargs -n 2 sh -c 'sed -i "$2s/<</
    << /g" $1 && sed -i "$2s/  <</ <</g" $1 && sed -i "$2s/<<  /<< /g" $1'
    sh

--- main/AgentDocs.cpp
+++ main/AgentDocs.cpp
@@ -146,7 +146,7 @@ void AgentDocs::generate(const std::string &docsdir, std::ostream &genStream) {
   "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied."
   "See the License for the specific language governing permissions and"
   "limitations under the License."
-  "-->"<<std::endl;
+  "-->" << std::endl;
   genStream << "# Processors" << std::endl << std::endl;
   genStream << "## Table of Contents" << std::endl << std::endl;
 
