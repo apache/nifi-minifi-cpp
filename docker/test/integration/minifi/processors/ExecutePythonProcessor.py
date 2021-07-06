2203f81cbcd33a738d0772fd90ad631a44082b01
Gabor Gyimesi <gamezbird@gmail.com>
2021-07-06 12:25:32 +0200

    Add test script file instead of inline python script

--- docker/test/integration/minifi/processors/ExecutePythonProcessor.py
+++ docker/test/integration/minifi/processors/ExecutePythonProcessor.py
@@ -3,17 +3,7 @@ from ..core.Processor import Processor
 
 class ExecutePythonProcessor(Processor):
     def __init__(self):
-        super(ExecutePythonProcessor, self).__init__('ExecutePythonProcessor',
-                                       properties={'Script Body':
-"""def describe(processor):
-    processor.setDescription("Adds an attribute to your flow files")
-
-def onInitialize(processor):
-  processor.setSupportsDynamicProperties()
-
-def onTrigger(context, session):
-  flow_file = session.get()
-  if flow_file is not None:
-    flow_file.addAttribute("Python attribute","attributevalue")
-    session.transfer(flow_file, REL_SUCCESS)"""},
-                                       auto_terminate=['success'])
+        super(ExecutePythonProcessor, self).__init__(
+            'ExecutePythonProcessor',
+            properties={'Script File': "/tmp/resources/python/add_attribute_to_flowfile.py"},
+            auto_terminate=['success'])
