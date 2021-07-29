74829310ce882198ef19f6476d6061330c54b0f4
aminadinari19 <aminadinari19@gmail.com>
2021-07-29 08:34:40 +0200

    MINIFICPP-1458 Register the hidden properties in InvokeHTTP

--- extensions/http-curl/processors/InvokeHTTP.cpp
+++ extensions/http-curl/processors/InvokeHTTP.cpp
@@ -163,6 +163,8 @@ void InvokeHTTP::initialize() {
   properties.insert(DisablePeerVerification);
   properties.insert(AlwaysOutputResponse);
   properties.insert(FollowRedirects);
+  properties.insert(PropPutOutputAttributes);
+  properties.insert(PenalizeOnNoRetry);
 
   setSupportedProperties(properties);
   // Set the supported relationships
@@ -358,12 +360,14 @@ void InvokeHTTP::onTrigger(const std::shared_ptr<core::ProcessContext> &context,
 
   // append all headers
   client.build_header_list(attribute_to_send_regex_, flowFile->getAttributes());
-
   logger_->log_trace("InvokeHTTP -- curl performed");
   if (client.submit()) {
     logger_->log_trace("InvokeHTTP -- curl successful");
 
     bool putToAttribute = !IsNullOrEmpty(put_attribute_name_);
+    if (putToAttribute) {
+      logger_->log_debug("Adding http response body to flow file attribute %s", put_attribute_name_);
+    }
 
     const std::vector<char> &response_body = client.getResponseBody();
     const std::vector<std::string> &response_headers = client.getHeaders();
@@ -444,6 +448,7 @@ void InvokeHTTP::route(const std::shared_ptr<core::FlowFile> &request, const std
   } else {
     if (request != nullptr) {
       if (penalize_no_retry_) {
+        logger_->log_debug("Flowfile has been penalized");
         session->penalize(request);
       }
       session->transfer(request, RelNoRetry);
