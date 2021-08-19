d1b104a32533fc336c980dae3aa528326cf15e3c
Gabor Gyimesi <gamezbird@gmail.com>
2021-08-19 13:44:22 +0200

    Fix formatting

--- extensions/azure/processors/PutAzureDataLakeStorage.cpp
+++ extensions/azure/processors/PutAzureDataLakeStorage.cpp
@@ -175,11 +175,11 @@ void PutAzureDataLakeStorage::onTrigger(const std::shared_ptr<core::ProcessConte
 }
 
 PutAzureDataLakeStorage::ReadCallback::ReadCallback(
-    uint64_t flow_size, storage::AzureDataLakeStorage& azure_data_lake_storage, const storage::PutAzureDataLakeStorageParameters& params, std::shared_ptr<logging::Logger> logger)
-  : flow_size_(flow_size)
-  , azure_data_lake_storage_(azure_data_lake_storage)
-  , params_(params)
-  , logger_(std::move(logger)) {
+      uint64_t flow_size, storage::AzureDataLakeStorage& azure_data_lake_storage, const storage::PutAzureDataLakeStorageParameters& params, std::shared_ptr<logging::Logger> logger)
+  : flow_size_(flow_size),
+    azure_data_lake_storage_(azure_data_lake_storage),
+    params_(params),
+    logger_(std::move(logger)) {
 }
 
 int64_t PutAzureDataLakeStorage::ReadCallback::process(const std::shared_ptr<io::BaseStream>& stream) {
