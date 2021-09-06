f1e71ed074116f57226b345c968c2a3786c41f52
Adam Debreceni <adebreceni@apache.org>
2021-09-06 10:44:16 +0200

    MINIFICPP-1632 - Add blank line

--- docker/test/integration/minifi/core/utils.py
+++ docker/test/integration/minifi/core/utils.py
@@ -17,6 +17,7 @@ def retry_check(max_tries=5, retry_interval=1):
         return retry_wrapper
     return retry_check_func
 
+
 def decode_escaped_str(str):
     # encode('ascii') makes sure that we don't mess up unicode characters
     # (as it throws if there are any)
