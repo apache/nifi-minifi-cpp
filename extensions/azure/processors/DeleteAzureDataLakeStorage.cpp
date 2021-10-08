040eee97d35d6b46206987935e6295b2e5fa8825
Gabor Gyimesi <gamezbird@gmail.com>
2021-10-08 14:40:00 +0200

    Fix log level

--- extensions/azure/processors/DeleteAzureDataLakeStorage.cpp
+++ extensions/azure/processors/DeleteAzureDataLakeStorage.cpp
@@ -56,7 +56,7 @@ std::optional<storage::DeleteAzureDataLakeStorageParameters> DeleteAzureDataLake
 }
 
 void DeleteAzureDataLakeStorage::onTrigger(const std::shared_ptr<core::ProcessContext>& context, const std::shared_ptr<core::ProcessSession>& session) {
-  logger_->log_debug("DeleteAzureDataLakeStorage onTrigger");
+  logger_->log_trace("DeleteAzureDataLakeStorage onTrigger");
   std::shared_ptr<core::FlowFile> flow_file = session->get();
   if (!flow_file) {
     context->yield();
