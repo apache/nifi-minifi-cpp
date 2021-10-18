c90df973858cbd20f3c528c3c5d3661b60202885
Martin Zink <martin.zink@protonmail.com>
2021-10-18 16:28:50 +0200

    flake8 fix

--- docker/test/integration/minifi/processors/DefragmentText.py
+++ docker/test/integration/minifi/processors/DefragmentText.py
@@ -4,6 +4,6 @@ from ..core.Processor import Processor
 class DefragmentText(Processor):
     def __init__(self, delimiter="<[0-9]+>", schedule={'scheduling period': '2 sec'}):
         super(DefragmentText, self).__init__('DefragmentText',
-                                                  schedule=schedule,
-                                                  properties={'Delimiter': delimiter},
-                                                  auto_terminate=['success', 'failure'])
+                                             schedule=schedule,
+                                             properties={'Delimiter': delimiter},
+                                             auto_terminate=['success', 'failure'])
