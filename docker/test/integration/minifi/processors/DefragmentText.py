67e92e9cf33a49d901e5e4a123df8d42e8fa142f
Martin Zink <martin.zink@protonmail.com>
2021-10-26 17:45:18 +0200

    Update docker/test/integration/minifi/processors/DefragmentText.py
    Co-authored-by: Ferenc Gerlits <fgerlits@users.noreply.github.com>

--- docker/test/integration/minifi/processors/DefragmentText.py
+++ docker/test/integration/minifi/processors/DefragmentText.py
@@ -2,7 +2,7 @@ from ..core.Processor import Processor
 
 
 class DefragmentText(Processor):
-    def __init__(self, delimiter="<[0-9]+>", schedule={'scheduling period': '2 sec'}):
+    def __init__(self, delimiter="<[0-9]+>", schedule={'scheduling strategy': 'EVENT_DRIVEN'}):
         super(DefragmentText, self).__init__('DefragmentText',
                                              schedule=schedule,
                                              properties={'Delimiter': delimiter},
