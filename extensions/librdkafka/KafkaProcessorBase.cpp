0f12783c3030079076c3c1025130750330414a56
Gabor Gyimesi <gamezbird@gmail.com>
2021-11-09 15:17:48 +0100

    Refactor and add documentation

--- extensions/librdkafka/KafkaProcessorBase.cpp
+++ extensions/librdkafka/KafkaProcessorBase.cpp
@@ -26,7 +26,7 @@ namespace org::apache::nifi::minifi::processors {
 
 const core::Property KafkaProcessorBase::SecurityProtocol(
         core::PropertyBuilder::createProperty("Security Protocol")
-        ->withDescription("Protocol used to communicate with brokers")
+        ->withDescription("Protocol used to communicate with brokers. Corresponds to Kafka's 'security.protocol' property.")
         ->withDefaultValue<std::string>(toString(SecurityProtocolOption::PLAINTEXT))
         ->withAllowableValues<std::string>(SecurityProtocolOption::values())
         ->isRequired(true)
@@ -36,10 +36,18 @@ const core::Property KafkaProcessorBase::SSLContextService(
         ->withDescription("SSL Context Service Name")
         ->asType<minifi::controllers::SSLContextService>()
         ->build());
-const core::Property KafkaProcessorBase::KerberosServiceName("Kerberos Service Name", "Kerberos Service Name", "");
-const core::Property KafkaProcessorBase::KerberosPrincipal("Kerberos Principal", "Keberos Principal", "");
-const core::Property KafkaProcessorBase::KerberosKeytabPath("Kerberos Keytab Path",
-                                                            "The path to the location on the local filesystem where the kerberos keytab is located. Read permission on the file is required.", "");
+const core::Property KafkaProcessorBase::KerberosServiceName(
+    core::PropertyBuilder::createProperty("Kerberos Service Name")
+        ->withDescription("Kerberos Service Name")
+        ->build());
+const core::Property KafkaProcessorBase::KerberosPrincipal(
+    core::PropertyBuilder::createProperty("Kerberos Principal")
+        ->withDescription("Keberos Principal")
+        ->build());
+const core::Property KafkaProcessorBase::KerberosKeytabPath(
+    core::PropertyBuilder::createProperty("Kerberos Keytab Path")
+        ->withDescription("The path to the location on the local filesystem where the kerberos keytab is located. Read permission on the file is required.")
+        ->build());
 const core::Property KafkaProcessorBase::SASLMechanism(
         core::PropertyBuilder::createProperty("SASL Mechanism")
         ->withDescription("The SASL mechanism to use for authentication. Corresponds to Kafka's 'sasl.mechanism' property.")
@@ -57,27 +65,26 @@ const core::Property KafkaProcessorBase::Password(
         ->build());
 
 std::optional<utils::SSL_data> KafkaProcessorBase::getSslData(core::ProcessContext* context) const {
-  utils::SSL_data ssl_data;
-
   std::string ssl_service_name;
   if (context->getProperty(SSLContextService.getName(), ssl_service_name) && !ssl_service_name.empty()) {
     std::shared_ptr<core::controller::ControllerService> service = context->getControllerService(ssl_service_name);
     if (service) {
       auto ssl_service = std::static_pointer_cast<minifi::controllers::SSLContextService>(service);
+      utils::SSL_data ssl_data;
       ssl_data.ca_loc = ssl_service->getCACertificate();
       ssl_data.cert_loc = ssl_service->getCertificateFile();
       ssl_data.key_loc = ssl_service->getPrivateKeyFile();
       ssl_data.key_pw = ssl_service->getPassphrase();
+      return ssl_data;
     } else {
       logger_->log_warn("SSL Context Service property is set to '%s', but the controller service could not be found.", ssl_service_name);
       return std::nullopt;
     }
   } else if (security_protocol_ == SecurityProtocolOption::SSL || security_protocol_ == SecurityProtocolOption::SASL_SSL) {
     logger_->log_warn("Security protocol is set to %s, but no valid SSL Context Service property is set.", security_protocol_.toString());
-    return std::nullopt;
   }
 
-  return ssl_data;
+  return std::nullopt;
 }
 
 void KafkaProcessorBase::setKafkaAuthenticationParameters(core::ProcessContext* context, rd_kafka_conf_t* config) {
@@ -87,17 +94,17 @@ void KafkaProcessorBase::setKafkaAuthenticationParameters(core::ProcessContext* 
   if (security_protocol_ == SecurityProtocolOption::SSL || security_protocol_ == SecurityProtocolOption::SASL_SSL) {
     auto ssl_data = getSslData(context);
     if (ssl_data) {
-      utils::setKafkaConfigurationField(*config, "ssl.ca.location", ssl_data->ca_loc);
-      logger_->log_debug("Kafka ssl.ca.location [%s]", ssl_data->ca_loc);
-      utils::setKafkaConfigurationField(*config, "ssl.certificate.location", ssl_data->cert_loc);
-      logger_->log_debug("Kafka ssl.certificate.location [%s]", ssl_data->cert_loc);
-      utils::setKafkaConfigurationField(*config, "ssl.key.location", ssl_data->key_loc);
-      logger_->log_debug("Kafka ssl.key.location [%s]", ssl_data->key_loc);
-      utils::setKafkaConfigurationField(*config, "ssl.key.password", ssl_data->key_pw);
-      logger_->log_debug("Kafka ssl.key.password was set");
-
       if (ssl_data->ca_loc.empty() && ssl_data->cert_loc.empty() && ssl_data->key_loc.empty() && ssl_data->key_pw.empty()) {
         logger_->log_warn("Security protocol is set to %s, but no valid security parameters are set in the properties or in the SSL Context Service.", security_protocol_.toString());
+      } else {
+        utils::setKafkaConfigurationField(*config, "ssl.ca.location", ssl_data->ca_loc);
+        logger_->log_debug("Kafka ssl.ca.location [%s]", ssl_data->ca_loc);
+        utils::setKafkaConfigurationField(*config, "ssl.certificate.location", ssl_data->cert_loc);
+        logger_->log_debug("Kafka ssl.certificate.location [%s]", ssl_data->cert_loc);
+        utils::setKafkaConfigurationField(*config, "ssl.key.location", ssl_data->key_loc);
+        logger_->log_debug("Kafka ssl.key.location [%s]", ssl_data->key_loc);
+        utils::setKafkaConfigurationField(*config, "ssl.key.password", ssl_data->key_pw);
+        logger_->log_debug("Kafka ssl.key.password was set");
       }
     }
   }
@@ -106,35 +113,23 @@ void KafkaProcessorBase::setKafkaAuthenticationParameters(core::ProcessContext* 
   utils::setKafkaConfigurationField(*config, "sasl.mechanism", sasl_mechanism.toString());
   logger_->log_debug("Kafka sasl.mechanism [%s]", sasl_mechanism.toString());
 
-  std::string value;
-  if (context->getProperty(KerberosServiceName.getName(), value) && !value.empty()) {
-    utils::setKafkaConfigurationField(*config, "sasl.kerberos.service.name", value);
-    logger_->log_debug("Kafka sasl.kerberos.service.name [%s]", value);
-  }
+  auto setKafkaConfigIfNotEmpty = [this, context, config](const std::string& property_name, const std::string& kafka_config_name, bool log_value = true) {
+    std::string value;
+    if (context->getProperty(property_name, value) && !value.empty()) {
+      utils::setKafkaConfigurationField(*config, kafka_config_name, value);
+      if (log_value) {
+        logger_->log_debug("Kafka %s [%s]", kafka_config_name, value);
+      } else {
+        logger_->log_debug("Kafka %s was set", kafka_config_name);
+      }
+    }
+  };
 
-  value = "";
-  if (context->getProperty(KerberosPrincipal.getName(), value) && !value.empty()) {
-    utils::setKafkaConfigurationField(*config, "sasl.kerberos.principal", value);
-    logger_->log_debug("Kafka sasl.kerberos.principal [%s]", value);
-  }
-
-  value = "";
-  if (context->getProperty(KerberosKeytabPath.getName(), value) && !value.empty()) {
-    utils::setKafkaConfigurationField(*config, "sasl.kerberos.keytab", value);
-    logger_->log_debug("Kafka sasl.kerberos.keytab [%s]", value);
-  }
-
-  value = "";
-  if (context->getProperty(Username.getName(), value) && !value.empty()) {
-    utils::setKafkaConfigurationField(*config, "sasl.username", value);
-    logger_->log_debug("Kafka sasl.username [%s]", value);
-  }
-
-  value = "";
-  if (context->getProperty(Password.getName(), value) && !value.empty()) {
-    utils::setKafkaConfigurationField(*config, "sasl.password", value);
-    logger_->log_debug("Kafka sasl.password was set");
-  }
+  setKafkaConfigIfNotEmpty(KerberosServiceName.getName(), "sasl.kerberos.service.name");
+  setKafkaConfigIfNotEmpty(KerberosPrincipal.getName(), "sasl.kerberos.principal");
+  setKafkaConfigIfNotEmpty(KerberosKeytabPath.getName(), "sasl.kerberos.keytab");
+  setKafkaConfigIfNotEmpty(Username.getName(), "sasl.username");
+  setKafkaConfigIfNotEmpty(Password.getName(), "sasl.password", false);
 }
 
 }  // namespace org::apache::nifi::minifi::processors
