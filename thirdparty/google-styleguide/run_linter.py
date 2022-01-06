ed0ecb7ffb2b8ced30cc94cc7b462def7ff90d14
Adam Markovics <nullzero89@gmail.com>
2022-01-06 09:47:34 +0100

    Review fixes 3

--- thirdparty/google-styleguide/run_linter.py
+++ thirdparty/google-styleguide/run_linter.py
@@ -35,21 +35,26 @@ def cpplint_main_wrapper(file_list):
         return err.code
 
 
-if __name__ == '__main__':
-    # break up list_of_files to ~equal chunks
-    chunk_num = multiprocessing.cpu_count()
-    chunk_size = math.ceil(len(list_of_files) / chunk_num)
-    chunks = []
+def create_chunks(chunk_size, items):
+    """breaks up items to equal chunks, with last being shorter if items are not evenly divisible by chunk_size"""
+    chunk_list = []
     chunk_begin = 0
     chunk_end = chunk_size
     for chunk_cnt in range(chunk_num):
-        chunks.append(list_of_files[chunk_begin:chunk_end])
+        chunk_list.append(items[chunk_begin:chunk_end])
         chunk_begin += chunk_size
         if chunk_begin >= len(list_of_files):
             break
         chunk_end += chunk_size
         if chunk_end > len(list_of_files):
             chunk_end = len(list_of_files)
+    return chunk_list
+
+
+if __name__ == '__main__':
+    chunk_num = multiprocessing.cpu_count()
+    chunk_max_size = math.ceil(len(list_of_files) / chunk_num)
+    chunks = create_chunks(chunk_max_size, list_of_files)
     pool = multiprocessing.Pool(processes=chunk_num)
     # execute in parallel
     map_res = pool.map(cpplint_main_wrapper, chunks)
