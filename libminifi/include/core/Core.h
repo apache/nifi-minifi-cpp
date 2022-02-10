ba2f25affaedabc9fa7aea1dd7c561f2eb90e880
Adam Debreceni <adebreceni@apache.org>
2022-02-10 16:56:08 +0100

    MINIFICPP-1748 - Remove struct prefix as well

--- libminifi/include/core/Core.h
+++ libminifi/include/core/Core.h
@@ -23,6 +23,7 @@
 
 #include <memory>
 #include <string>
+#include <string_view>
 
 #ifdef WIN32
 #pragma comment(lib, "shlwapi.lib")
@@ -80,13 +81,16 @@ static inline std::string getClassName() {
   std::free(b);
   return name;
 #else
-  std::string adjusted_name = typeid(T).name();
-  // can probably skip class  manually for slightly higher performance
-  const std::string clazz = "class ";
-  auto haz_clazz = adjusted_name.find(clazz);
-  if (haz_clazz == 0)
-    adjusted_name = adjusted_name.substr(clazz.length(), adjusted_name.length() - clazz.length());
-  return adjusted_name;
+  const std::string_view name = typeid(T).name();
+  const std::string_view class_prefix = "class ";
+  const std::string_view struct_prefix = "struct ";
+
+  if (name.find(class_prefix) == 0) {
+    name.remove_prefix(class_prefix.length());
+  } else if (name.find(struct_prefix) == 0) {
+    name.remove_prefix(struct_prefix.length());
+  }
+  return name;
 #endif
 }
 
