acc2d6a1225874cdc528da3987b76383351a80a6
Martin Zink <martinzink@apache.org>
2022-04-14 09:32:07 +0200

    Enable Expression Languaage tests on Windows

--- extensions/expression-language/tests/ExpressionLanguageTests.cpp
+++ extensions/expression-language/tests/ExpressionLanguageTests.cpp
@@ -1176,7 +1176,6 @@ TEST_CASE("Encode Decode CSV", "[expressionEncodeDecodeCSV]") {
   REQUIRE("Zero > One < \"two!\" & 'true'" == expr(expression::Parameters{ flow_file_a }).asString());
 }
 
-#ifndef WIN32
 #ifndef DISABLE_CURL
 TEST_CASE("Encode URL", "[expressionEncodeURL]") {
   auto expr = expression::compile("${message:urlEncode()}");
@@ -1225,7 +1224,6 @@ TEST_CASE("Encode Decode URL", "[expressionEncodeDecodeURLExcept]") {
   flow_file_a->addAttribute("message", "some value with spaces");
   REQUIRE_THROWS(expr(expression::Parameters{flow_file_a}).asString());
 }
-#endif
 #endif
 
 #ifdef EXPRESSION_LANGUAGE_USE_DATE
