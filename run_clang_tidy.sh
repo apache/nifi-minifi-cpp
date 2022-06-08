f3cb54ae1476514380068c2937b4a457378798b1
Ferenc Gerlits <fgerlits@gmail.com>
2022-06-08 10:07:08 +0200

    MINIFICPP-1858 Exclude unbuilt extensions and increase timeout

--- run_clang_tidy.sh
+++ run_clang_tidy.sh
@@ -5,7 +5,14 @@ set -uo pipefail
 exit_code=0
 FILES=$1
 
+EXCLUDED_EXTENSIONS=("pdh" "windows-event-log" "tensorflow")
+
 for changed_file in ${FILES}; do
+  for extension in "${EXCLUDED_EXTENSIONS[@]}"; do
+    if [[ "${changed_file}" =~ extensions/${extension}/ ]]; then
+      continue 2
+    fi
+  done
   if [[ "${changed_file}" == *.cpp ]]; then
     clang-tidy-14 -warnings-as-errors=* -quiet -p build "${changed_file}"
     exit_code=$(( $? | exit_code ))
