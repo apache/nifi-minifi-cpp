c9a16273a9e06872490137e62858db22722cd97f
Martin Zink <martinzink@apache.org>
2022-06-21 15:28:09 +0200

    MINIFICPP-1761 Enable multithreading in PutGCSObject, DeleteGCSObject, FetchGCSObject
    Signed-off-by: Gabor Gyimesi <gamezbird@gmail.com>
    
    This closes #1368

--- extensions/gcp/processors/DeleteGCSObject.h
+++ extensions/gcp/processors/DeleteGCSObject.h
@@ -55,7 +55,7 @@ class DeleteGCSObject : public GCSProcessor {
   EXTENSIONAPI static constexpr bool SupportsDynamicProperties = false;
   EXTENSIONAPI static constexpr bool SupportsDynamicRelationships = false;
   EXTENSIONAPI static constexpr core::annotation::Input InputRequirement = core::annotation::Input::INPUT_REQUIRED;
-  EXTENSIONAPI static constexpr bool IsSingleThreaded = true;
+  EXTENSIONAPI static constexpr bool IsSingleThreaded = false;
 
   ADD_COMMON_VIRTUAL_FUNCTIONS_FOR_PROCESSORS
 
--- extensions/gcp/processors/FetchGCSObject.h
+++ extensions/gcp/processors/FetchGCSObject.h
@@ -56,7 +56,7 @@ class FetchGCSObject : public GCSProcessor {
   EXTENSIONAPI static constexpr bool SupportsDynamicProperties = false;
   EXTENSIONAPI static constexpr bool SupportsDynamicRelationships = false;
   EXTENSIONAPI static constexpr core::annotation::Input InputRequirement = core::annotation::Input::INPUT_REQUIRED;
-  EXTENSIONAPI static constexpr bool IsSingleThreaded = true;
+  EXTENSIONAPI static constexpr bool IsSingleThreaded = false;
 
   ADD_COMMON_VIRTUAL_FUNCTIONS_FOR_PROCESSORS
 
