f47646801950c5c3ddacfe33b62b7928bbebca83
Ferenc Gerlits <fgerlits@gmail.com>
2022-07-21 14:18:06 +0200

    simplify the Kubernetes API call by using genericClient

--- extensions/kubernetes/MetricsApi.cpp
+++ extensions/kubernetes/MetricsApi.cpp
@@ -19,64 +19,30 @@
 #include <memory>
 
 extern "C" {
-#include "include/apiClient.h"
-#include "include/list.h"
+#include "include/generic.h"
 }
 
 #include "ApiClient.h"
-#include "utils/StringUtils.h"
 
 namespace {
-struct list_t_deleter {
-  void operator()(list_t* ptr) const noexcept { list_freeList(ptr); }
+struct genericClient_t_deleter {
+  void operator()(genericClient_t* ptr) const noexcept { genericClient_free(ptr); }
 };
-using list_unique_ptr = std::unique_ptr<list_t, list_t_deleter>;
+using genericClient_unique_ptr = std::unique_ptr<genericClient_t, genericClient_t_deleter>;
 }  // namespace
 
 namespace org::apache::nifi::minifi::kubernetes::metrics {
 
 nonstd::expected<std::string, std::string> podMetricsList(const kubernetes::ApiClient& api_client) {
-  list_unique_ptr localVarQueryParameters;
-  list_unique_ptr localVarHeaderParameters;
-  list_unique_ptr localVarFormParameters;
-  list_unique_ptr localVarHeaderType{list_createList()};
-  list_unique_ptr localVarContentType;
-  char* localVarBodyParameters = nullptr;
-
-  std::string url_path = "/apis/metrics.k8s.io/v1beta1/pods";
-  char* localVarPath = const_cast<char*>(url_path.c_str());
-
-  std::string header_type = "application/json";
-  list_addElement(localVarHeaderType.get(), const_cast<char*>(header_type.c_str()));
-
-  std::string verb = "GET";
-  char* request_type = const_cast<char*>(verb.c_str());
-
-  apiClient_invoke(api_client.getClient(),
-                  localVarPath,
-                  localVarQueryParameters.get(),
-                  localVarHeaderParameters.get(),
-                  localVarFormParameters.get(),
-                  localVarHeaderType.get(),
-                  localVarContentType.get(),
-                  localVarBodyParameters,
-                  request_type);
-
-  std::string result;
-  if (api_client.getClient()->dataReceived) {
-    result = std::string{static_cast<const char*>(api_client.getClient()->dataReceived)};
-    free(api_client.getClient()->dataReceived);
-    api_client.getClient()->dataReceived = nullptr;
-    api_client.getClient()->dataReceivedLen = 0;
+  genericClient_unique_ptr genericClient{genericClient_create(api_client.getClient(), "metrics.k8s.io", "v1beta1", "pods")};
+  char* api_response = Generic_list(genericClient.get());
+  if (api_response) {
+    std::string result{api_response};
+    free(api_response);
+    return result;
+  } else {
+    return nonstd::make_unexpected("Could not access the Kubernetes API /apis/metrics.k8s.io/v1beta1/pods");
   }
-
-  const auto is_success = [](auto response_code) { return response_code >= 200 && response_code < 300; };
-  if (!is_success(api_client.getClient()->response_code)) {
-    const auto error_code = std::to_string(api_client.getClient()->response_code);
-    return nonstd::make_unexpected(utils::StringUtils::join_pack("Error from Kubernetes API ", url_path, ": error code ", error_code, ", data received: ", result));
-  }
-
-  return result;
 }
 
 }  // namespace org::apache::nifi::minifi::kubernetes::metrics
