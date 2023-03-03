7942e0e7f344d449bb8b4635b1a8737caf57bce5
Gabor Gyimesi <gamezbird@gmail.com>
2023-03-03 14:16:42 +0100

    MINIFICPP-2053 Reduce CentOS image size in CI build
    It started to occur that the CentOS build exceeded the 14GB disk space
    Github Actions VMs provide (for example
    https://github.com/apache/nifi-minifi-cpp/actions/runs/4263514879/jobs/7420351598).
    JDK and the packaged tar.gz are removed from the CI build and JNI and
    SFTP builds are moved to the ubuntu build. This reduces the image size
    by about 2GBs.
    
    Closes #1513
    Signed-off-by: Marton Szasz <szaszm@apache.org>

--- cmake/DockerConfig.cmake
+++ cmake/DockerConfig.cmake
@@ -65,6 +65,19 @@ add_custom_target(
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/docker/)
 
 add_custom_target(
+    centos-test
+    COMMAND ${CMAKE_SOURCE_DIR}/docker/DockerBuild.sh
+        -u 1000
+        -g 1000
+        -v ${PROJECT_VERSION_MAJOR}.${PROJECT_VERSION_MINOR}.${PROJECT_VERSION_PATCH}
+        -o ${MINIFI_DOCKER_OPTIONS_STR}
+        -d centos
+        -c BUILD_NUMBER=${BUILD_NUMBER}
+        -c DOCKER_CCACHE_DUMP_LOCATION=${DOCKER_CCACHE_DUMP_LOCATION}
+        -c DOCKER_SKIP_TESTS=OFF
+    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/docker/)
+
+add_custom_target(
     fedora
     COMMAND ${CMAKE_SOURCE_DIR}/docker/DockerBuild.sh
         -u 1000
