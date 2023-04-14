72e1c35164ab19137fc19396f902536c625584a5
Martin Zink <martinzink@apache.org>
2023-04-14 14:34:58 +0200

    MINIFICPP-2103 JNI extension compile fix (libc++)
    Signed-off-by: Gabor Gyimesi <gamezbird@gmail.com>
    
    This closes #1561

--- extensions/jni/jvm/JniReferenceObjects.h
+++ extensions/jni/jvm/JniReferenceObjects.h
@@ -104,7 +104,7 @@ class JniByteInputStream {
     }
 
     // seek to offset
-    auto remaining = gsl::narrow<uint64_t>(size);
+    auto remaining = gsl::narrow<size_t>(size);
     int writtenOffset = 0;
     int read = 0;
     do {
@@ -128,8 +128,11 @@ class JniByteInputStream {
     return read;
   }
 
-  int64_t read(uint8_t &arr) {
-    return stream_->read(arr);
+  int64_t read(uint8_t& value) const {
+    auto read_result = stream_->read(value);
+    if (io::isError(read_result))
+      return -1;
+    return gsl::narrow<int64_t>(read_result);
   }
 
   std::shared_ptr<minifi::io::InputStream> stream_;
@@ -153,9 +156,9 @@ class JniInputStream : public core::WeakReference {
     }
   }
 
-  int64_t read(uint8_t &arr) {
+  int64_t read(uint8_t& value) {
     if (!removed_) {
-      return jbi_->read(arr);
+      return jbi_->read(value);
     }
     return -1;
   }
