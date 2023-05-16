cad8df471de1c2a60a4a7b975853556815954c8a
Adam Debreceni <adebreceni@apache.org>
2023-05-16 23:49:11 +0200

    MINIFICPP-2020 - Lock file implementation

---
+++ libminifi/include/utils/FileMutex.h
@@ -0,0 +1,45 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <filesystem>
+#include <array>
+#include <mutex>
+
+namespace org::apache::nifi::minifi::utils {
+
+class FileMutex {
+ public:
+  explicit FileMutex(std::filesystem::path path);
+
+  FileMutex(const FileMutex&) = delete;
+  FileMutex(FileMutex&&) = delete;
+  FileMutex& operator=(const FileMutex&) = delete;
+  FileMutex& operator=(FileMutex&&) = delete;
+
+  void lock();
+  void unlock();
+
+ private:
+  std::filesystem::path path_;
+
+  std::mutex mtx_;
+  std::array<std::byte, 24> file_handle_;
+};
+
+}  // namespace org::apache::nifi::minifi::utils
---
+++ libminifi/src/utils/FileMutex.cpp
@@ -0,0 +1,116 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "utils/FileMutex.h"
+
+#include <optional>
+#include "utils/gsl.h"
+
+template<typename T>
+static T& getOsHandle(std::array<std::byte, 24>& file_handle) {
+  void* ptr = (void*)file_handle.data();
+  size_t size = file_handle.size();
+  void* result = std::align(alignof(T), sizeof(T), ptr, size);
+  gsl_Assert(result);
+  return *reinterpret_cast<T*>(result);
+}
+
+#ifdef WIN32
+#include <windows.h>
+#include "utils/OsUtils.h"
+
+namespace org::apache::nifi::minifi::utils {
+
+FileMutex::FileMutex(std::filesystem::path path) {
+  path_ = std::move(path);
+
+}
+
+void FileMutex::lock() {
+  std::lock_guard guard(mtx_);
+  HANDLE handle = CreateFileA(path_.string().c_str(), (GENERIC_READ | GENERIC_WRITE), 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+  if (handle == INVALID_HANDLE_VALUE) {
+    throw std::runtime_error("Failed to open file '" + path_.string() + "' to be locked: " + utils::OsUtils::windowsErrorToErrorCode(GetLastError()).message());
+  }
+
+  getOsHandle<HANDLE>(data_) = handle;
+}
+
+void FileMutex::unlock() {
+  std::lock_guard guard(mtx_);
+  CloseHandle(*cast<HANDLE>(data_));
+}
+
+}  // namespace org::apache::nifi::minifi::utils
+
+#else
+
+#include <fcntl.h>
+#include <unistd.h>
+
+namespace org::apache::nifi::minifi::utils {
+
+void FileMutex::lock() {
+  std::lock_guard guard(mtx_);
+  int flags = O_RDWR | O_CREAT;
+#ifdef O_CLOEXEC
+  flags |= O_CLOEXEC;
+#endif
+  int fd = open(path_.string().c_str(), flags, 0644);
+  if (fd < 0) {
+    throw std::runtime_error("Failed to open file '" + path_.string() + "' to be locked: " + std::strerror(errno));
+  }
+
+  errno = 0;
+  struct flock f;
+  memset(&f, 0, sizeof(f));
+  f.l_type = F_WRLCK;
+  f.l_whence = SEEK_SET;
+  f.l_start = 0;
+  f.l_len = 0;
+  int value = fcntl(fd, F_SETLK, &f);
+  if (value == -1) {
+    std::string err_str = "Failed to lock file '" + path_.string() + "': " + std::strerror(errno);
+    close(fd);
+    throw std::runtime_error(err_str);
+  }
+
+  getOsHandle<int>(file_handle_) = fd;
+}
+
+void FileMutex::unlock() {
+  std::lock_guard guard(mtx_);
+  errno = 0;
+  struct flock f;
+  memset(&f, 0, sizeof(f));
+  f.l_type = F_UNLCK;
+  f.l_whence = SEEK_SET;
+  f.l_start = 0;
+  f.l_len = 0;
+  int value = fcntl(getOsHandle<int>(file_handle_), F_SETLK, &f);
+  if (value == -1) {
+    std::string err_str = "Failed to unlock file '" + path_.string() + "': " + std::strerror(errno);
+    close(getOsHandle<int>(file_handle_));
+    throw std::runtime_error(err_str);
+  }
+  close(getOsHandle<int>(file_handle_));
+}
+
+}  // namespace org::apache::nifi::minifi::utils
+
+#endif
