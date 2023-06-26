3baf862d2e5ff354bdea12f64aea71cda800fbb5
Ferenc Gerlits <fgerlits@gmail.com>
2023-06-26 11:18:15 +0200

    MINIFICPP-2145 Parallelize the clang-tidy CI job
    Also: enable the JNI extension in the clang-tidy CI job.

--- run_clang_tidy.sh
+++ run_clang_tidy.sh
@@ -2,27 +2,29 @@
 
 set -uo pipefail
 
-exit_code=0
-FILES=$1
+FILE=$1
 
 EXCLUDED_EXTENSIONS=("pdh" "windows-event-log" "tensorflow")
 EXCLUDED_DIRECTORY=("nanofi")
 
-for changed_file in ${FILES}; do
-  for excluded_extension in "${EXCLUDED_EXTENSIONS[@]}"; do
-    if [[ "${changed_file}" =~ extensions/${excluded_extension}/ ]]; then
-      continue 2
-    fi
-  done
-  for excluded_directory in "${EXCLUDED_DIRECTORY[@]}"; do
-    if [[ "${changed_file}" =~ ${excluded_directory}/ ]]; then
-      continue 2
-    fi
-  done
-  if [[ "${changed_file}" == *.cpp ]] && [[ -f "${changed_file}" ]]; then
-    clang-tidy-14 -warnings-as-errors=* -quiet -p build "${changed_file}"
-    exit_code=$(( $? | exit_code ))
-  fi;
+for excluded_extension in "${EXCLUDED_EXTENSIONS[@]}"; do
+  if [[ "${FILE}" =~ extensions/${excluded_extension}/ ]]; then
+    exit 0
+  fi
 done
 
-exit $exit_code
+for excluded_directory in "${EXCLUDED_DIRECTORY[@]}"; do
+  if [[ "${FILE}" =~ ${excluded_directory}/ ]]; then
+    exit 0
+  fi
+done
+
+if ! [[ "${FILE}" == *.cpp ]]; then
+  exit 0
+fi
+
+if ! [[ -f "${FILE}" ]]; then
+  exit 0
+fi
+
+clang-tidy-14 -warnings-as-errors=* -quiet -p build "$FILE"
