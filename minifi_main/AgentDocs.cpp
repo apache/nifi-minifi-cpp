0389e80d60dfc12519b793411203968d98f524d3
Ferenc Gerlits <fgerlits@users.noreply.github.com>
2023-09-22 16:48:22 +0200

    use a raw string literal for the license string
    Co-authored-by: Márton Szász <szaszm01@gmail.com>

--- minifi_main/AgentDocs.cpp
+++ minifi_main/AgentDocs.cpp
@@ -73,20 +73,20 @@ std::string formatListOfRelationships(std::span<const minifi::core::Relationship
   return minifi::utils::StringUtils::join(", ", relationships, [](const auto& relationship) { return relationship.name; });
 }
 
-inline constexpr std::string_view APACHE_LICENSE = "<!--\n"
-    "Licensed to the Apache Software Foundation (ASF) under one or more\n"
-    "contributor license agreements.  See the NOTICE file distributed with\n"
-    "this work for additional information regarding copyright ownership.\n"
-    "The ASF licenses this file to You under the Apache License, Version 2.0\n"
-    "(the \"License\"); you may not use this file except in compliance with\n"
-    "the License.  You may obtain a copy of the License at\n"
-    "    http://www.apache.org/licenses/LICENSE-2.0\n"
-    "Unless required by applicable law or agreed to in writing, software\n"
-    "distributed under the License is distributed on an \"AS IS\" BASIS,\n"
-    "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"
-    "See the License for the specific language governing permissions and\n"
-    "limitations under the License.\n"
-    "-->";
+inline constexpr std::string_view APACHE_LICENSE = R"license(<!--
+Licensed to the Apache Software Foundation (ASF) under one or more
+contributor license agreements.  See the NOTICE file distributed with
+this work for additional information regarding copyright ownership.
+The ASF licenses this file to You under the Apache License, Version 2.0
+(the "License"); you may not use this file except in compliance with
+the License.  You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->)license";
 
 void writeHeader(std::ostream& docs, const std::vector<std::pair<std::string, minifi::ClassDescription>>& class_descriptions) {
   docs << APACHE_LICENSE;
