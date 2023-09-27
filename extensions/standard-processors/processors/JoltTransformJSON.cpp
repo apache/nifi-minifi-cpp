78b9a39a8b57607bfb1239d974d94df5c824ae87
Adam Debreceni <adebreceni@apache.org>
2023-09-27 13:03:02 +0200

    MINIFICPP-2217 - Parse jolt specification

---
+++ extensions/standard-processors/processors/JoltTransformJSON.cpp
@@ -0,0 +1,380 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "JoltTransformJSON.h"
+#include "core/Resource.h"
+#include "utils/ProcessorConfigUtils.h"
+#include "rapidjson/document.h"
+#include "rapidjson/error/en.h"
+#include "utils/StringUtils.h"
+
+namespace org::apache::nifi::minifi::processors {
+
+void JoltTransformJSON::initialize() {
+  setSupportedProperties(Properties);
+  setSupportedRelationships(Relationships);
+}
+
+void JoltTransformJSON::onSchedule(core::ProcessContext* context, core::ProcessSessionFactory* /*session_factory*/) {
+  gsl_Expects(context);
+  transform_ = utils::parseEnumProperty<jolt_transform_json::JoltTransform>(*context, JoltTransform);
+  const std::string spec_str = utils::getRequiredPropertyOrThrow(*context, JoltSpecification.name);
+  if (auto spec = Spec::parse(spec_str)) {
+    spec_ = std::move(spec.value());
+  } else {
+    throw Exception(PROCESS_SCHEDULE_EXCEPTION, fmt::format("The value of '{}' is not a valid jolt specification: {}", JoltSpecification.name, spec.error()));
+  }
+}
+
+void JoltTransformJSON::onTrigger(core::ProcessContext* context, core::ProcessSession* session) {
+  gsl_Expects(context && session);
+  auto flowfile = session->get();
+  if (!flowfile) {
+    context->yield();
+    return;
+  }
+
+
+}
+
+nonstd::expected<JoltTransformJSON::Spec::Template, std::string> JoltTransformJSON::Spec::Template::parse(std::string_view str, const std::string& escapables) {
+  enum class State {
+    Plain,
+    Escaped,
+    Template,  // &
+    SimpleIndex,  // &1
+    CanonicalTemplate,  // &(
+    ParentIndex,  // &(1
+    NextIndex,  // &(1,
+    MatchIndex  // &(1,0
+  };
+
+  JoltTransformJSON::Spec::Template res;
+  res.fragments.push_back({});
+  State state = State::Plain;
+  std::string target;
+  // go beyond the last char on purpose
+  for (size_t ch_idx = 0; ch_idx <= str.size(); ++ch_idx) {
+    std::optional<char> ch;
+    if (ch_idx < str.size()) {
+      ch = str[ch_idx];
+    }
+    switch (state) {
+      case State::Plain: {
+        if (ch == '\\') {
+          state = State::Escaped;
+        } else if (ch == '&') {
+          res.references.push_back({});
+          res.fragments.push_back({});
+          state = State::Template;
+        } else if (ch) {
+          res.fragments.back() += ch.value();
+        }
+        break;
+      }
+      case State::Escaped: {
+        if (!ch) {
+          return nonstd::make_unexpected("Unterminated escape sequence");
+        }
+        if (!(ch == '\\' || ch == '&' || std::find(escapables.begin(), escapables.end(), ch.value()) != escapables.end())) {
+          return nonstd::make_unexpected(fmt::format("Unknown escape sequence in template '\\{}'", ch.value()));
+        }
+        res.fragments.back() += ch.value();
+        state = State::Plain;
+        break;
+      }
+      case State::Template: {
+        if (ch == '(') {
+          state = State::CanonicalTemplate;
+        } else if (ch && std::isdigit(static_cast<unsigned char>(ch.value()))) {
+          target.clear();
+          target += std::to_string(ch.value());
+          state = State::SimpleIndex;
+        } else {
+          state = State::Plain;
+          // reprocess this char in a different state
+          --ch_idx;
+        }
+        break;
+      }
+      case State::SimpleIndex: {
+        if (ch && std::isdigit(static_cast<unsigned char>(ch.value()))) {
+          target += std::to_string(ch.value());
+        } else {
+          res.references.back().first = std::stoi(target);
+          state = State::Plain;
+          // reprocess this char in a different state
+          --ch_idx;
+        }
+        break;
+      }
+      case State::CanonicalTemplate: {
+        if (ch && std::isdigit(static_cast<unsigned char>(ch.value()))) {
+          target.clear();
+          target += std::to_string(ch.value());
+          state = State::ParentIndex;
+        } else {
+          return nonstd::make_unexpected(fmt::format("Expected an index at {}", ch_idx));
+        }
+        break;
+      }
+      case State::ParentIndex: {
+        if (ch && std::isdigit(static_cast<unsigned char>(ch.value()))) {
+          target += std::to_string(ch.value());
+        } else if (ch == ',') {
+          res.references.back().first = std::stoi(target);
+          state = State::NextIndex;
+        } else if (ch == ')') {
+          res.references.back().first = std::stoi(target);
+          state = State::Plain;
+        } else {
+          return nonstd::make_unexpected(fmt::format("Invalid character at {}, expected digit, comma or close parenthesis", ch_idx));
+        }
+        break;
+      }
+      case State::NextIndex: {
+        if (ch && std::isdigit(static_cast<unsigned char>(ch.value()))) {
+          target.clear();
+          target += std::to_string(ch.value());
+          state = State::MatchIndex;
+        } else {
+          return nonstd::make_unexpected(fmt::format("Expected an index at {}", ch_idx));
+        }
+        break;
+      }
+      case State::MatchIndex: {
+        if (ch && std::isdigit(static_cast<unsigned char>(ch.value()))) {
+          target += std::to_string(ch.value());
+        } else if (ch == ')') {
+          res.references.back().second = std::stoi(target);
+          state = State::Plain;
+        } else {
+          return nonstd::make_unexpected(fmt::format("Invalid character at {}, expected digit or close parenthesis", ch_idx));
+        }
+        break;
+      }
+    }
+  }
+
+  gsl_Assert(state == State::Plain);
+  return res;
+}
+
+nonstd::expected<JoltTransformJSON::Spec::Regex, std::string> JoltTransformJSON::Spec::Regex::parse(std::string_view str, const std::string& escapables) {
+  enum class State {
+    Plain,
+    Escaped
+  };
+  JoltTransformJSON::Spec::Regex res;
+  res.fragments.push_back({});
+  State state = State::Plain;
+  for (size_t idx = 0; idx <= str.size(); ++idx) {
+    std::optional<char> ch;
+    if (idx < str.size()) {
+      ch = str[idx];
+    }
+    switch (state) {
+      case State::Plain: {
+        if (ch == '\\') {
+          state = State::Escaped;
+        } else if (ch == '*') {
+          res.fragments.push_back({});
+        } else if (ch) {
+          res.fragments.back() += ch.value();
+        }
+        break;
+      }
+      case State::Escaped: {
+        if (!ch) {
+          return nonstd::make_unexpected("Unterminated escape sequence");
+        }
+        if (!(ch == '\\' || ch == '&' || std::find(escapables.begin(), escapables.end(), ch.value()) != escapables.end())) {
+          return nonstd::make_unexpected(fmt::format("Unknown escape sequence in pattern '\\{}'", ch.value()));
+        }
+        res.fragments.back() += ch.value();
+        state = State::Plain;
+        break;
+      }
+    }
+  }
+  gsl_Assert(state == State::Plain);
+  return res;
+}
+
+std::string JoltTransformJSON::Spec::Template::eval(const Context& ctx) const {
+  std::string res;
+  for (size_t idx = 0; idx + 1 < fragments.size(); ++idx) {
+    res += fragments.at(idx);
+    auto& ref = references.at(idx);
+    auto* target = ctx.find(ref.first);
+    if (!target) {
+      throw Exception(GENERAL_EXCEPTION, fmt::format("Invalid reference to {}", ref.first));
+    }
+    if (target->matches.size() <= ref.second) {
+      throw Exception(GENERAL_EXCEPTION, fmt::format("Could not find match {} in '{}'", ref.second, target->matches.at(0)));
+    }
+    res += target->matches.at(ref.second);
+  }
+  res += fragments.back();
+  return res;
+}
+
+std::optional<std::vector<std::string_view>> JoltTransformJSON::Spec::Regex::match(std::string_view str) const {
+  std::vector<std::string_view> matches;
+  auto it = str.begin();
+  for (size_t idx = 0; idx < fragments.size(); ++idx) {
+    auto& frag = fragments[idx];
+    auto next_it = std::search(it, str.end(), frag.begin(), frag.end());
+    if (next_it == str.end()) {
+      return std::nullopt;
+    }
+    if (idx == 0 && next_it != str.begin()) {
+      return std::nullopt;
+    }
+    matches.push_back({it, next_it});
+    it = next_it + frag.size();
+  }
+  if (it != str.end()) {
+    return std::nullopt;
+  }
+  return matches;
+}
+
+namespace {
+
+JoltTransformJSON::Spec::Destinations parseDestinations(const JoltTransformJSON::Spec::Context& ctx, const rapidjson::Value& val);
+
+JoltTransformJSON::Spec::Pattern::Value parseValue(const JoltTransformJSON::Spec::Context& ctx, const rapidjson::Value& val);
+
+std::unique_ptr<JoltTransformJSON::Spec::Pattern> parseMap(const JoltTransformJSON::Spec::Context& ctx, const rapidjson::Value& val) {
+  if (!val.IsObject()) {
+    throw Exception(GENERAL_EXCEPTION, fmt::format("Expected a map at '{}'", ctx.path()));
+  }
+  auto map = std::make_unique<JoltTransformJSON::Spec::Pattern>();
+
+  for (auto it = val.MemberBegin(); it != val.MemberEnd(); ++it) {
+    std::string_view name{it->name.GetString(), it->name.GetStringLength()};
+    JoltTransformJSON::Spec::Context sub_ctx{.parent = &ctx, .member = name};
+    if (name == "@") {
+      map->self = parseDestinations(sub_ctx, it->value);
+    } else if (name == "$") {
+      map->value = parseDestinations(sub_ctx, it->value);
+    } else {
+      auto templ = JoltTransformJSON::Spec::Template::parse(name, "*");
+      auto reg = JoltTransformJSON::Spec::Regex::parse(name, "&");
+      if (templ && reg) {
+        throw Exception(GENERAL_EXCEPTION, "Pattern cannot contain both & and *");
+      }
+      if (templ) {
+        map->templates.insert({templ.value(), parseValue(sub_ctx, it->value)});
+      } else if (reg) {
+        map->regexes.insert({reg.value(), parseValue(sub_ctx, it->value)});
+      } else {
+        map->literals.insert({std::string{name}, parseValue(sub_ctx, it->value)});
+      }
+    }
+  }
+  return map;
+}
+
+std::vector<JoltTransformJSON::Spec::Template> parseDestination(const JoltTransformJSON::Spec::Context& ctx, const rapidjson::Value& val) {
+  if (!val.IsString()) {
+    throw Exception(GENERAL_EXCEPTION, fmt::format("Expected a string or array of strings at '{}'", ctx.path()));
+  }
+
+  enum class State {
+    Plain,
+    Escaped
+  };
+
+  std::vector<JoltTransformJSON::Spec::Template> result;
+  std::string_view str{val.GetString(), val.GetStringLength()};
+  State state = State::Plain;
+  size_t segment_begin = 0;
+  for (size_t idx = 0; idx <= str.size(); ++idx) {
+    std::optional<char> ch;
+    if (idx < str.size()) {
+      ch = str[idx];
+    }
+    switch (state) {
+      case State::Plain: {
+        if (ch == '\\') {
+          state = State::Escaped;
+        } else if (!ch || ch == '.') {
+          result.push_back(JoltTransformJSON::Spec::Template::parse(str.substr(segment_begin, idx - segment_begin), ".").value());
+          segment_begin = idx + 1;
+        }
+        break;
+      }
+      case State::Escaped: {
+        if (!ch) {
+          throw Exception(GENERAL_EXCEPTION, "Unterminated escape sequence in destination");
+        }
+        if (!(ch == '\\' || ch == '.' || ch == '&')) {
+          throw Exception(GENERAL_EXCEPTION, fmt::format("Unknown escape sequence in destination '\\{}'", ch.value()));
+        }
+        break;
+      }
+    }
+  }
+
+  gsl_Assert(state == State::Plain);
+
+  return result;
+}
+
+JoltTransformJSON::Spec::Destinations parseDestinations(const JoltTransformJSON::Spec::Context& ctx, const rapidjson::Value& val) {
+  JoltTransformJSON::Spec::Destinations res;
+  if (val.IsArray()) {
+    for (rapidjson::SizeType i = 0; i < val.GetArray().Size(); ++i) {
+      std::string idx_str = std::to_string(i);
+      JoltTransformJSON::Spec::Context sub_ctx{.parent = &ctx, .member = idx_str};
+      res.push_back(parseDestination(sub_ctx, val.GetArray()[i]));
+    }
+  } else {
+    res.push_back(parseDestination(ctx, val));
+  }
+  return res;
+}
+
+
+JoltTransformJSON::Spec::Pattern::Value parseValue(const JoltTransformJSON::Spec::Context& ctx, const rapidjson::Value& val) {
+  if (val.IsObject()) {
+    return parseMap(ctx, val);
+  }
+  return parseDestinations(ctx, val);
+}
+
+}  // namespace
+
+nonstd::expected<JoltTransformJSON::Spec, std::string> JoltTransformJSON::Spec::parse(std::string_view str) {
+  rapidjson::Document doc;
+  rapidjson::ParseResult res = doc.Parse(str.data(), str.length());
+  if (!res) {
+    return nonstd::make_unexpected(fmt::format("{} at {}", rapidjson::GetParseError_En(res.Code()), res.Offset()));
+  }
+  try {
+    Spec::Context ctx;
+    return Spec{parseMap(ctx, doc)};
+  } catch (const std::exception& ex) {
+    return nonstd::make_unexpected(ex.what());
+  }
+}
+
+REGISTER_RESOURCE(JoltTransformJSON, Processor);
+
+}  // namespace org::apache::nifi::minifi::processors
---
+++ extensions/standard-processors/processors/JoltTransformJSON.h
@@ -0,0 +1,178 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <string>
+
+#include "core/Processor.h"
+#include "core/ProcessSession.h"
+#include "core/PropertyDefinition.h"
+#include "core/PropertyDefinitionBuilder.h"
+#include "utils/Enum.h"
+#include "utils/Searcher.h"
+
+namespace org::apache::nifi::minifi::processors::jolt_transform_json {
+enum class JoltTransform {
+  SHIFT
+};
+}  // namespace org::apache::nifi::minifi::extensions::gcp
+
+namespace magic_enum::customize {
+using JoltTransform = org::apache::nifi::minifi::processors::jolt_transform_json::JoltTransform;
+
+template <>
+constexpr customize_t enum_name<JoltTransform>(JoltTransform value) noexcept {
+  switch (value) {
+    case JoltTransform::SHIFT:
+      return "Shift";
+  }
+  return invalid_tag;
+}
+}  // namespace magic_enum::customize
+
+namespace org::apache::nifi::minifi::processors {
+
+class JoltTransformJSON : public core::Processor {
+ public:
+  explicit JoltTransformJSON(std::string_view name, const utils::Identifier& uuid = {})
+      : Processor(name, uuid) {}
+
+
+  EXTENSIONAPI static constexpr const char* Description = "Applies a list of Jolt specifications to the flowfile JSON payload. A new FlowFile is created "
+      "with transformed content and is routed to the 'success' relationship. If the JSON transform "
+      "fails, the original FlowFile is routed to the 'failure' relationship.";
+
+  EXTENSIONAPI static constexpr auto JoltTransform = core::PropertyDefinitionBuilder<magic_enum::enum_count<jolt_transform_json::JoltTransform>()>::createProperty("Jolt Transformation DSL")
+      .withDescription("Specifies the Jolt Transformation that should be used with the provided specification.")
+      .withDefaultValue(magic_enum::enum_name(jolt_transform_json::JoltTransform::SHIFT))
+      .withAllowedValues(magic_enum::enum_names<jolt_transform_json::JoltTransform>())
+      .isRequired(true)
+      .build();
+
+  EXTENSIONAPI static constexpr auto JoltSpecification = core::PropertyDefinitionBuilder<>::createProperty("Jolt Specification")
+      .withDescription("Jolt Specification for transformation of JSON data. The value for this property may be the text of a Jolt specification "
+          "or the path to a file containing a Jolt specification. 'Jolt Specification' must be set, or "
+          "the value is ignored if the Jolt Sort Transformation is selected.")
+      .supportsExpressionLanguage(true)
+      .isRequired(true)
+      .build();
+
+  EXTENSIONAPI static constexpr auto Properties = std::array<core::PropertyReference, 2>{
+      JoltTransform,
+      JoltSpecification
+  };
+
+  EXTENSIONAPI static constexpr auto Success = core::RelationshipDefinition{"success", "The FlowFile with transformed content will be routed to this relationship"};
+  EXTENSIONAPI static constexpr auto Failure = core::RelationshipDefinition{"failure", "If a FlowFile fails processing for any reason (for example, the FlowFile is not valid JSON), it will be routed to this relationship"};
+  EXTENSIONAPI static constexpr auto Relationships = std::array{Success, Failure};
+
+  EXTENSIONAPI static constexpr bool SupportsDynamicProperties = false;
+  EXTENSIONAPI static constexpr bool SupportsDynamicRelationships = false;
+  EXTENSIONAPI static constexpr core::annotation::Input InputRequirement = core::annotation::Input::INPUT_REQUIRED;
+  EXTENSIONAPI static constexpr bool IsSingleThreaded = false;
+
+  ADD_COMMON_VIRTUAL_FUNCTIONS_FOR_PROCESSORS
+
+  void onSchedule(core::ProcessContext* context, core::ProcessSessionFactory* session_factory) override;
+  void onTrigger(core::ProcessContext* context, core::ProcessSession* session) override;
+  void initialize() override;
+
+  class Spec {
+   public:
+    struct Context {
+     public:
+      const Context* parent{nullptr};
+
+      std::string path() const {
+        std::string res;
+        if (parent) {
+          res = parent->path();
+        }
+        res.append("/").append(member);
+        return res;
+      }
+
+      const Context* find(size_t idx) const {
+        if (idx == 0) return this;
+        if (parent) return parent->find(idx - 1);
+        return nullptr;
+      }
+
+      std::string_view member;
+      std::vector<std::string_view> matches;
+    };
+
+    class Template {
+      Template() = default;
+     public:
+      static nonstd::expected<Template, std::string> parse(std::string_view str, const std::string& escapables);
+
+      std::string eval(const Context& ctx) const;
+
+      auto operator<=>(const Template& other) const {
+        return full <=> other.full;
+      }
+
+     private:
+      std::vector<std::string> fragments;
+      std::vector<std::pair<size_t, size_t>> references;
+      std::string full;
+    };
+
+    class Regex {
+      Regex() = default;
+     public:
+      static nonstd::expected<Regex, std::string> parse(std::string_view str, const std::string& escapables);
+
+      std::optional<std::vector<std::string_view>> match(std::string_view str) const;
+
+      auto operator<=>(const Regex& other) const {
+        return full <=> other.full;
+      }
+
+     private:
+      std::vector<std::string> fragments;
+      std::string full;
+    };
+
+    using Destinations = std::vector<std::vector<Template>>;
+
+    struct Pattern {
+      using Value = std::variant<std::unique_ptr<Pattern>, Destinations>;
+
+      std::map<std::string, Value> literals;
+      std::map<Template, Value> templates;  // '&'
+      std::map<Regex, Value> regexes;  // '*'
+      std::optional<Destinations> self;  // '@'
+      std::optional<Destinations> value;  // '$'
+    };
+
+    static nonstd::expected<Spec, std::string> parse(std::string_view str);
+
+   private:
+    explicit Spec(std::unique_ptr<Pattern> value): value_(std::move(value)) {}
+
+    std::unique_ptr<Pattern> value_;
+  };
+
+ private:
+  jolt_transform_json::JoltTransform transform_;
+  std::optional<Spec> spec_;
+};
+
+}  // namespace org::apache::nifi::minifi::processors
