28cee7ece3fcadfc0117909c908180a6052fd35f
Marton Szasz <szaszm@apache.org>
2023-11-22 14:15:09 +0100

    MINIFICPP-2266 avoid using system versions of fmt and spdlog
    Signed-off-by: Ferenc Gerlits <fgerlits@gmail.com>
    This closes #1697

--- cmake/Spdlog.cmake
+++ cmake/Spdlog.cmake
@@ -22,6 +22,6 @@ set(SPDLOG_FMT_EXTERNAL ON CACHE STRING "" FORCE)
 FetchContent_Declare(Spdlog
         URL  https://github.com/gabime/spdlog/archive/refs/tags/v1.12.0.tar.gz
         URL_HASH SHA256=4dccf2d10f410c1e2feaff89966bfc49a1abb29ef6f08246335b110e001e09a9
-        FIND_PACKAGE_ARGS
+        OVERRIDE_FIND_PACKAGE
         )
 FetchContent_MakeAvailable(Spdlog)
