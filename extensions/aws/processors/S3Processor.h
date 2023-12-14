e1f23f464a73d2266ea1233da49fb79cc59f22ba
Marton Szasz <szaszm@apache.org>
2023-12-14 15:35:28 +0100

    MINIFICPP-2271 update AWS SDK and regions, port patches
    Signed-off-by: Marton Szasz <szaszm@apache.org>

--- extensions/aws/processors/S3Processor.h
+++ extensions/aws/processors/S3Processor.h
@@ -48,6 +48,7 @@ inline constexpr std::string_view AP_NORTHEAST_1 = "ap-northeast-1";
 inline constexpr std::string_view AP_NORTHEAST_2 = "ap-northeast-2";
 inline constexpr std::string_view AP_NORTHEAST_3 = "ap-northeast-3";
 inline constexpr std::string_view AP_SOUTH_1 = "ap-south-1";
+inline constexpr std::string_view AP_SOUTH_2 = "ap-south-2";
 inline constexpr std::string_view AP_SOUTHEAST_1 = "ap-southeast-1";
 inline constexpr std::string_view AP_SOUTHEAST_2 = "ap-southeast-2";
 inline constexpr std::string_view AP_SOUTHEAST_3 = "ap-southeast-3";
@@ -55,8 +56,10 @@ inline constexpr std::string_view CA_CENTRAL_1 = "ca-central-1";
 inline constexpr std::string_view CN_NORTH_1 = "cn-north-1";
 inline constexpr std::string_view CN_NORTHWEST_1 = "cn-northwest-1";
 inline constexpr std::string_view EU_CENTRAL_1 = "eu-central-1";
+inline constexpr std::string_view EU_CENTRAL_2 = "eu-central-2";
 inline constexpr std::string_view EU_NORTH_1 = "eu-north-1";
 inline constexpr std::string_view EU_SOUTH_1 = "eu-south-1";
+inline constexpr std::string_view EU_SOUTH_2 = "eu-south-2";
 inline constexpr std::string_view EU_WEST_1 = "eu-west-1";
 inline constexpr std::string_view EU_WEST_2 = "eu-west-2";
 inline constexpr std::string_view EU_WEST_3 = "eu-west-3";
@@ -73,12 +76,14 @@ inline constexpr std::string_view US_ISO_WEST_1 = "us-iso-west-1";
 inline constexpr std::string_view US_WEST_1 = "us-west-1";
 inline constexpr std::string_view US_WEST_2 = "us-west-2";
 
-inline constexpr auto REGIONS = std::array{region::AF_SOUTH_1, region::AP_EAST_1, region::AP_NORTHEAST_1,
-  region::AP_NORTHEAST_2, region::AP_NORTHEAST_3, region::AP_SOUTH_1, region::AP_SOUTHEAST_1, region::AP_SOUTHEAST_2,
-  region::AP_SOUTHEAST_3, region::CA_CENTRAL_1, region::CN_NORTH_1, region::CN_NORTHWEST_1, region::EU_CENTRAL_1,
-  region::EU_NORTH_1, region::EU_SOUTH_1, region::EU_WEST_1, region::EU_WEST_2, region::EU_WEST_3, region::ME_CENTRAL_1,
-  region::ME_SOUTH_1, region::SA_EAST_1, region::US_EAST_1, region::US_EAST_2, region::US_GOV_EAST_1, region::US_GOV_WEST_1,
-  region::US_ISO_EAST_1, region::US_ISOB_EAST_1, region::US_ISO_WEST_1, region::US_WEST_1, region::US_WEST_2};
+inline constexpr auto REGIONS = std::array{
+  AF_SOUTH_1, AP_EAST_1, AP_NORTHEAST_1,
+  AP_NORTHEAST_2, AP_NORTHEAST_3, AP_SOUTH_1, AP_SOUTH_2, AP_SOUTHEAST_1, AP_SOUTHEAST_2,
+  AP_SOUTHEAST_3, CA_CENTRAL_1, CN_NORTH_1, CN_NORTHWEST_1, EU_CENTRAL_1, EU_CENTRAL_2,
+  EU_NORTH_1, EU_SOUTH_1, EU_SOUTH_2, EU_WEST_1, EU_WEST_2, EU_WEST_3, ME_CENTRAL_1,
+  ME_SOUTH_1, SA_EAST_1, US_EAST_1, US_EAST_2, US_GOV_EAST_1, US_GOV_WEST_1,
+  US_ISO_EAST_1, US_ISOB_EAST_1, US_ISO_WEST_1, US_WEST_1, US_WEST_2
+};
 }  // namespace region
 
 struct CommonProperties {
