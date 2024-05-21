02fc3b0e132ddaf3ed6c55a2afd37f383c89e0e8
james94 <jamesmedel94@gmail.com>
2024-05-20 20:58:51 -0700

    Verified Can Install & Build MiNiFi w OpenSSL, CURL, ZLIB & run minifi binary via Conan v2

--- cmake/BundledZLIB.cmake
+++ cmake/BundledZLIB.cmake
@@ -18,56 +18,65 @@
 function(use_bundled_zlib SOURCE_DIR BINARY_DIR)
     message("Using bundled zlib")
 
-    # Define byproducts
-    if (WIN32)
-        string(TOLOWER "${CMAKE_BUILD_TYPE}" build_type)
-        if (build_type MATCHES relwithdebinfo OR build_type MATCHES release)
-            set(BYPRODUCT "lib/zlibstatic.lib")
+    if(USE_CONAN_PACKAGER)
+        message("Using Conan Packager to manage installing prebuilt zlib external lib")
+        include(${CMAKE_BINARY_DIR}/ZLIBConfig.cmake)
+
+    elseif(USE_STANDALONE_CMAKE)
+        message("Using CMAKE's ExternalProject_Add to manage source building zlib external lib")
+    
+        # Define byproducts
+        if (WIN32)
+            string(TOLOWER "${CMAKE_BUILD_TYPE}" build_type)
+            if (build_type MATCHES relwithdebinfo OR build_type MATCHES release)
+                set(BYPRODUCT "lib/zlibstatic.lib")
+            else()
+                set(BYPRODUCT "lib/zlibstaticd.lib")
+            endif()
         else()
-            set(BYPRODUCT "lib/zlibstaticd.lib")
+            set(BYPRODUCT "lib/libz.a")
         endif()
-    else()
-        set(BYPRODUCT "lib/libz.a")
+
+        # Set build options
+        set(ZLIB_CMAKE_ARGS ${PASSTHROUGH_CMAKE_ARGS}
+                "-DCMAKE_INSTALL_PREFIX=${BINARY_DIR}/thirdparty/zlib-install"
+                )
+
+        # Build project
+        ExternalProject_Add(
+            zlib-external
+            URL "https://github.com/madler/zlib/archive/v1.2.11.tar.gz"
+            URL_HASH "SHA256=629380c90a77b964d896ed37163f5c3a34f6e6d897311f1df2a7016355c45eff"
+            SOURCE_DIR "${BINARY_DIR}/thirdparty/zlib-src"
+            CMAKE_ARGS ${ZLIB_CMAKE_ARGS}
+            BUILD_BYPRODUCTS "${BINARY_DIR}/thirdparty/zlib-install/${BYPRODUCT}"
+            EXCLUDE_FROM_ALL TRUE
+        )
+
+        # Set variables
+        set(ZLIB_FOUND "YES" CACHE STRING "" FORCE)
+        set(ZLIB_INCLUDE_DIRS "${BINARY_DIR}/thirdparty/zlib-install/include" CACHE STRING "" FORCE)
+        set(ZLIB_LIBRARIES "${BINARY_DIR}/thirdparty/zlib-install/${BYPRODUCT}" CACHE STRING "" FORCE)
+        set(ZLIB_VERSION_STRING "1.2.11" CACHE STRING "" FORCE)
+        set(ZLIB_VERSION_MAJOR 1 CACHE STRING "" FORCE)
+        set(ZLIB_VERSION_MINOR 2 CACHE STRING "" FORCE)
+        set(ZLIB_VERSION_PATCH 11 CACHE STRING "" FORCE)
+
+        # Set exported variables for FindPackage.cmake
+        set(PASSTHROUGH_VARIABLES ${PASSTHROUGH_VARIABLES} "-DEXPORTED_ZLIB_INCLUDE_DIRS=${ZLIB_INCLUDE_DIRS}" CACHE STRING "" FORCE)
+        set(PASSTHROUGH_VARIABLES ${PASSTHROUGH_VARIABLES} "-DEXPORTED_ZLIB_LIBRARIES=${ZLIB_LIBRARIES}" CACHE STRING "" FORCE)
+        set(PASSTHROUGH_VARIABLES ${PASSTHROUGH_VARIABLES} "-DEXPORTED_ZLIB_VERSION_STRING=${ZLIB_VERSION_STRING}" CACHE STRING "" FORCE)
+        set(PASSTHROUGH_VARIABLES ${PASSTHROUGH_VARIABLES} "-DEXPORTED_ZLIB_VERSION_MAJOR=${ZLIB_VERSION_MAJOR}" CACHE STRING "" FORCE)
+        set(PASSTHROUGH_VARIABLES ${PASSTHROUGH_VARIABLES} "-DEXPORTED_ZLIB_VERSION_MINOR=${ZLIB_VERSION_MINOR}" CACHE STRING "" FORCE)
+        set(PASSTHROUGH_VARIABLES ${PASSTHROUGH_VARIABLES} "-DEXPORTED_ZLIB_VERSION_PATCH=${ZLIB_VERSION_PATCH}" CACHE STRING "" FORCE)
+
+        # Create imported targets
+        file(MAKE_DIRECTORY ${ZLIB_INCLUDE_DIRS})
+
+        add_library(ZLIB::ZLIB STATIC IMPORTED)
+        set_target_properties(ZLIB::ZLIB PROPERTIES IMPORTED_LOCATION "${ZLIB_LIBRARIES}")
+        add_dependencies(ZLIB::ZLIB zlib-external)
+        set_property(TARGET ZLIB::ZLIB APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${ZLIB_INCLUDE_DIRS})
+
     endif()
-
-    # Set build options
-    set(ZLIB_CMAKE_ARGS ${PASSTHROUGH_CMAKE_ARGS}
-            "-DCMAKE_INSTALL_PREFIX=${BINARY_DIR}/thirdparty/zlib-install"
-            )
-
-    # Build project
-    ExternalProject_Add(
-        zlib-external
-        URL "https://github.com/madler/zlib/archive/v1.2.11.tar.gz"
-        URL_HASH "SHA256=629380c90a77b964d896ed37163f5c3a34f6e6d897311f1df2a7016355c45eff"
-        SOURCE_DIR "${BINARY_DIR}/thirdparty/zlib-src"
-        CMAKE_ARGS ${ZLIB_CMAKE_ARGS}
-        BUILD_BYPRODUCTS "${BINARY_DIR}/thirdparty/zlib-install/${BYPRODUCT}"
-        EXCLUDE_FROM_ALL TRUE
-    )
-
-    # Set variables
-    set(ZLIB_FOUND "YES" CACHE STRING "" FORCE)
-    set(ZLIB_INCLUDE_DIRS "${BINARY_DIR}/thirdparty/zlib-install/include" CACHE STRING "" FORCE)
-    set(ZLIB_LIBRARIES "${BINARY_DIR}/thirdparty/zlib-install/${BYPRODUCT}" CACHE STRING "" FORCE)
-    set(ZLIB_VERSION_STRING "1.2.11" CACHE STRING "" FORCE)
-    set(ZLIB_VERSION_MAJOR 1 CACHE STRING "" FORCE)
-    set(ZLIB_VERSION_MINOR 2 CACHE STRING "" FORCE)
-    set(ZLIB_VERSION_PATCH 11 CACHE STRING "" FORCE)
-
-    # Set exported variables for FindPackage.cmake
-    set(PASSTHROUGH_VARIABLES ${PASSTHROUGH_VARIABLES} "-DEXPORTED_ZLIB_INCLUDE_DIRS=${ZLIB_INCLUDE_DIRS}" CACHE STRING "" FORCE)
-    set(PASSTHROUGH_VARIABLES ${PASSTHROUGH_VARIABLES} "-DEXPORTED_ZLIB_LIBRARIES=${ZLIB_LIBRARIES}" CACHE STRING "" FORCE)
-    set(PASSTHROUGH_VARIABLES ${PASSTHROUGH_VARIABLES} "-DEXPORTED_ZLIB_VERSION_STRING=${ZLIB_VERSION_STRING}" CACHE STRING "" FORCE)
-    set(PASSTHROUGH_VARIABLES ${PASSTHROUGH_VARIABLES} "-DEXPORTED_ZLIB_VERSION_MAJOR=${ZLIB_VERSION_MAJOR}" CACHE STRING "" FORCE)
-    set(PASSTHROUGH_VARIABLES ${PASSTHROUGH_VARIABLES} "-DEXPORTED_ZLIB_VERSION_MINOR=${ZLIB_VERSION_MINOR}" CACHE STRING "" FORCE)
-    set(PASSTHROUGH_VARIABLES ${PASSTHROUGH_VARIABLES} "-DEXPORTED_ZLIB_VERSION_PATCH=${ZLIB_VERSION_PATCH}" CACHE STRING "" FORCE)
-
-    # Create imported targets
-    file(MAKE_DIRECTORY ${ZLIB_INCLUDE_DIRS})
-
-    add_library(ZLIB::ZLIB STATIC IMPORTED)
-    set_target_properties(ZLIB::ZLIB PROPERTIES IMPORTED_LOCATION "${ZLIB_LIBRARIES}")
-    add_dependencies(ZLIB::ZLIB zlib-external)
-    set_property(TARGET ZLIB::ZLIB APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${ZLIB_INCLUDE_DIRS})
 endfunction(use_bundled_zlib)
