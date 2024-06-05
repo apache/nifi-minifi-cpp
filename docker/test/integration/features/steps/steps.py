d874f8c8aa5fe2a42b413792644573b486cc40db
Gabor Gyimesi <gamezbird@gmail.com>
2024-06-05 16:16:58 +0200

    MINIFICPP-2369 Test parameters with expression language

--- docker/test/integration/features/steps/steps.py
+++ docker/test/integration/features/steps/steps.py
@@ -1288,3 +1288,15 @@ def step_impl(context, install_mode):
 @given("the example MiNiFi python processors are present")
 def step_impl(context):
     context.test.enable_example_minifi_python_processors()
+
+
+@given("a non-sensitive parameter in the flow config called '{parameter_name}' with the value '{parameter_value}' in the parameter context '{parameter_context_name}'")
+def step_impl(context, parameter_context_name, parameter_name, parameter_value):
+    container = context.test.acquire_container(context=context, name='minifi-cpp-flow', engine='minifi-cpp')
+    container.add_parameter_to_flow_config(parameter_context_name, parameter_name, parameter_value)
+
+
+@given("parameter context name is set to '{parameter_context_name}'")
+def step_impl(context, parameter_context_name):
+    container = context.test.acquire_container(context=context, name='minifi-cpp-flow', engine='minifi-cpp')
+    container.set_parameter_context_name(parameter_context_name)
