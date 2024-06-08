6312cf008c4a4167241b39765ae3fe8f2ccc73c7
james94 <jamesmedel94@gmail.com>
2024-06-08 14:58:33 -0700

    Replaced MINIFI_USE_CONAN_PACKAGER w MINIFI_{ZLIB,OPENSSL,LIBCURL}_SOURCE vars
    Was able to build MiNiFi C++ with the ENABLE_{External_Lib} variables set to OFF initially.
    However, after a suggestion from Marton to leave the ENABLE external lib variables set to
    their default values from MiNiFiOptions.cmake, I removed the tc.variables['ENABLE_{...}']
    that overrode MiNiFiOptions.cmake ones and now am trying to build MiNiFi C++ using conan
    where we use conan's OpenSSL, ZLib and LibCURL prebuilt conan packages alternative to building
    them from source. I am debugging issues that come up from building MiNiFi using conan where
    partly we use conan's prebuilt conan packages and then the remaining external libs are the ones
    built from source. For instance, one finding related to needing to use the predefined conan profile
    located in etc/conan/profiles/release-linux because of the gnu20 was required to build rocksdb
    successfully. I found that for my environment, conan's generated default profile used gnu17,
    which caused rocksdb to fail building, but setting conan to use gnu20 allowed it to succeed in
    building rocksdb.

--- conanfile.py
+++ conanfile.py
@@ -19,52 +19,9 @@ class MiNiFiCppMain(ConanFile):
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["MINIFI_USE_CONAN_PACKAGER"] = "ON"
-
-        tc.variables["ENABLE_OPENWSMAN"] = "OFF"
-        tc.variables["ENABLE_CIVET"] = "OFF"
-        tc.variables["SKIP_TESTS"] = "ON"
-        tc.variables["ENABLE_EXPRESSION_LANGUAGE"] = "OFF"
-        tc.variables["ENABLE_BZIP2"] = "OFF"
-        tc.variables["ENABLE_ROCKSDB"] = "OFF"
-        tc.variables["BUILD_ROCKSDB"] = "OFF"
-
-        if self.settings.os == "Windows":
-            tc.variables["ENABLE_WEL"] = "OFF"
-            tc.variables["ENABLE_PDH"] = "OFF"
-            tc.variables["ENABLE_SMB"] = "OFF"
-        elif self.settings.os == "Linux":
-            tc.variables["ENABLE_SYSTEMD"] = "OFF"
-            tc.variables["ENABLE_PROCFS"] = "OFF"
-
-        tc.variables["ENABLE_LIBARCHIVE"] = "OFF"
-        tc.variables["ENABLE_LZMA"] = "OFF"
-        tc.variables["ENABLE_GPS"] = "OFF"
-        tc.variables["ENABLE_COAP"] = "OFF"
-        tc.variables["ENABLE_SQL"] = "OFF"
-        tc.variables["ENABLE_MQTT"] = "OFF"
-        tc.variables["ENABLE_PCAP"] = "OFF"
-        tc.variables["ENABLE_LIBRDKAFKA"] = "OFF"
-        tc.variables["ENABLE_LUA_SCRIPTING"] = "OFF"
-        tc.variables["ENABLE_PYTHON_SCRIPTING"] = "OFF"
-        tc.variables["ENABLE_SENSORS"] = "OFF"
-        tc.variables["ENABLE_USB_CAMERA"] = "OFF"
-        tc.variables["ENABLE_AWS"] = "OFF"
-        tc.variables["ENABLE_OPENCV"] = "OFF"
-        tc.variables["ENABLE_BUSTACHE"] = "OFF"
-        tc.variables["ENABLE_SFTP"] = "OFF"
-        tc.variables["ENABLE_AZURE"] = "OFF"
-        tc.variables["ENABLE_ENCRYPT_CONFIG"] = "OFF"
-        tc.variables["ENABLE_SPLUNK"] = "OFF"
-        tc.variables["ENABLE_ELASTICSEARCH"] = "OFF"
-        tc.variables["ENABLE_GCP"] = "OFF"
-        tc.variables["ENABLE_KUBERNETES"] = "OFF"
-        tc.variables["ENABLE_TEST_PROCESSORS"] = "OFF"
-        tc.variables["ENABLE_PROMETHEUS"] = "OFF"
-        tc.variables["ENABLE_GRAFANA_LOKI"] = "OFF"
-        tc.variables["ENABLE_GRPC_FOR_LOKI"] = "OFF"
-        tc.variables["ENABLE_CONTROLLER"] = "OFF"
-
+        tc.variables["MINIFI_LIBCURL_SOURCE"] = "CONAN"
+        tc.variables["MINIFI_OPENSSL_SOURCE"] = "CONAN"
+        tc.variables["MINIFI_ZLIB_SOURCE"] = "CONAN"
         tc.generate()
 
     def build(self):
