29bdc8ea2ce584c3179ab0ee07ed4424559eb66e
Ferenc Gerlits <fgerlits@gmail.com>
2024-06-17 12:03:27 +0200

    MINIFICPP-2351 Fix flakiness in MultipartUploadStateStorageTest
    Closes #1798
    
    Signed-off-by: Martin Zink <martinzink@apache.org>

--- extensions/aws/tests/MultipartUploadStateStorageTest.cpp
+++ extensions/aws/tests/MultipartUploadStateStorageTest.cpp
@@ -44,53 +44,33 @@ class MultipartUploadStateStorageTestFixture {
 
 TEST_CASE_METHOD(MultipartUploadStateStorageTestFixture, "Store and get current key state", "[s3StateStorage]") {
   REQUIRE(upload_storage_->getState("test_bucket", "key") == std::nullopt);
-  minifi::aws::s3::MultipartUploadState state;
-  state.upload_id = "id1";
+  minifi::aws::s3::MultipartUploadState state("id1", 50_MiB, 200_MiB, Aws::Utils::DateTime::Now());
   state.uploaded_parts = 2;
   state.uploaded_size = 100_MiB;
-  state.part_size = 50_MiB;
-  state.full_size = 200_MiB;
-  state.upload_time = Aws::Utils::DateTime::CurrentTimeMillis();
   state.uploaded_etags = {"etag1", "etag2"};
   upload_storage_->storeState("test_bucket", "key", state);
   REQUIRE(*upload_storage_->getState("test_bucket", "key") == state);
 }
 
 TEST_CASE_METHOD(MultipartUploadStateStorageTestFixture, "Get key upload state from multiple keys and buckets", "[s3StateStorage]") {
-  minifi::aws::s3::MultipartUploadState state1;
-  state1.upload_id = "id1";
+  minifi::aws::s3::MultipartUploadState state1("id1", 50_MiB, 200_MiB, Aws::Utils::DateTime::Now());
   state1.uploaded_parts = 3;
   state1.uploaded_size = 150_MiB;
-  state1.part_size = 50_MiB;
-  state1.full_size = 200_MiB;
-  state1.upload_time = Aws::Utils::DateTime::CurrentTimeMillis();
   state1.uploaded_etags = {"etag1", "etag2", "etag3"};
   upload_storage_->storeState("old_bucket", "key1", state1);
-  minifi::aws::s3::MultipartUploadState state2;
-  state2.upload_id = "id2";
+  minifi::aws::s3::MultipartUploadState state2("id2", 50_MiB, 200_MiB, Aws::Utils::DateTime::Now());
   state2.uploaded_parts = 2;
   state2.uploaded_size = 100_MiB;
-  state2.part_size = 50_MiB;
-  state2.full_size = 200_MiB;
-  state2.upload_time = Aws::Utils::DateTime::CurrentTimeMillis();
   state2.uploaded_etags = {"etag4", "etag5"};
   upload_storage_->storeState("test_bucket", "key1", state2);
-  minifi::aws::s3::MultipartUploadState state3;
-  state3.upload_id = "id3";
+  minifi::aws::s3::MultipartUploadState state3("id3", 50_MiB, 400_MiB, Aws::Utils::DateTime::Now());
   state3.uploaded_parts = 4;
   state3.uploaded_size = 200_MiB;
-  state3.part_size = 50_MiB;
-  state3.full_size = 400_MiB;
-  state3.upload_time = Aws::Utils::DateTime::CurrentTimeMillis();
   state3.uploaded_etags = {"etag6", "etag7", "etag9", "etag8"};
   upload_storage_->storeState("test_bucket", "key2", state3);
-  minifi::aws::s3::MultipartUploadState state4;
-  state2.upload_id = "id2";
+  minifi::aws::s3::MultipartUploadState state4("id2", 50_MiB, 200_MiB, Aws::Utils::DateTime::Now());
   state2.uploaded_parts = 3;
   state2.uploaded_size = 150_MiB;
-  state2.part_size = 50_MiB;
-  state2.full_size = 200_MiB;
-  state2.upload_time = Aws::Utils::DateTime::CurrentTimeMillis();
   state2.uploaded_etags = {"etag4", "etag5", "etag10"};
   upload_storage_->storeState("test_bucket", "key1", state4);
   REQUIRE(*upload_storage_->getState("test_bucket", "key1") == state4);
@@ -99,31 +79,19 @@ TEST_CASE_METHOD(MultipartUploadStateStorageTestFixture, "Get key upload state f
 }
 
 TEST_CASE_METHOD(MultipartUploadStateStorageTestFixture, "Remove state", "[s3StateStorage]") {
-  minifi::aws::s3::MultipartUploadState state1;
-  state1.upload_id = "id1";
+  minifi::aws::s3::MultipartUploadState state1("id1", 50_MiB, 200_MiB, Aws::Utils::DateTime::Now());
   state1.uploaded_parts = 3;
   state1.uploaded_size = 150_MiB;
-  state1.part_size = 50_MiB;
-  state1.full_size = 200_MiB;
-  state1.upload_time = Aws::Utils::DateTime::CurrentTimeMillis();
   state1.uploaded_etags = {"etag1", "etag2", "etag3"};
   upload_storage_->storeState("old_bucket", "key1", state1);
-  minifi::aws::s3::MultipartUploadState state2;
-  state2.upload_id = "id2";
+  minifi::aws::s3::MultipartUploadState state2("id2", 50_MiB, 200_MiB, Aws::Utils::DateTime::Now());
   state2.uploaded_parts = 2;
   state2.uploaded_size = 100_MiB;
-  state2.part_size = 50_MiB;
-  state2.full_size = 200_MiB;
-  state2.upload_time = Aws::Utils::DateTime::CurrentTimeMillis();
   state2.uploaded_etags = {"etag4", "etag5"};
   upload_storage_->storeState("test_bucket", "key1", state2);
-  minifi::aws::s3::MultipartUploadState state3;
-  state3.upload_id = "id3";
+  minifi::aws::s3::MultipartUploadState state3("id3", 50_MiB, 400_MiB, Aws::Utils::DateTime::Now());
   state3.uploaded_parts = 4;
   state3.uploaded_size = 200_MiB;
-  state3.part_size = 50_MiB;
-  state3.full_size = 400_MiB;
-  state3.upload_time = Aws::Utils::DateTime::CurrentTimeMillis();
   state3.uploaded_etags = {"etag6", "etag7", "etag9", "etag8"};
   upload_storage_->storeState("test_bucket", "key2", state3);
   REQUIRE(*upload_storage_->getState("old_bucket", "key1") == state1);
@@ -136,25 +104,18 @@ TEST_CASE_METHOD(MultipartUploadStateStorageTestFixture, "Remove state", "[s3Sta
 }
 
 TEST_CASE_METHOD(MultipartUploadStateStorageTestFixture, "Remove aged off state", "[s3StateStorage]") {
-  minifi::aws::s3::MultipartUploadState state1;
-  state1.upload_id = "id1";
+  using namespace std::literals::chrono_literals;
+  minifi::aws::s3::MultipartUploadState state1("id1", 50_MiB, 200_MiB, Aws::Utils::DateTime::Now() - 10min);
   state1.uploaded_parts = 3;
   state1.uploaded_size = 150_MiB;
-  state1.part_size = 50_MiB;
-  state1.full_size = 200_MiB;
-  state1.upload_time = Aws::Utils::DateTime(Aws::Utils::DateTime::CurrentTimeMillis()) - std::chrono::minutes(10);
   state1.uploaded_etags = {"etag1", "etag2", "etag3"};
   upload_storage_->storeState("test_bucket", "key1", state1);
-  minifi::aws::s3::MultipartUploadState state2;
-  state2.upload_id = "id2";
+  minifi::aws::s3::MultipartUploadState state2("id2", 50_MiB, 200_MiB, Aws::Utils::DateTime::Now());
   state2.uploaded_parts = 2;
   state2.uploaded_size = 100_MiB;
-  state2.part_size = 50_MiB;
-  state2.full_size = 200_MiB;
-  state2.upload_time = Aws::Utils::DateTime::CurrentTimeMillis();
   state2.uploaded_etags = {"etag4", "etag5"};
   upload_storage_->storeState("test_bucket", "key2", state2);
-  upload_storage_->removeAgedStates(std::chrono::milliseconds(10));
+  upload_storage_->removeAgedStates(10min);
   REQUIRE(upload_storage_->getState("test_bucket", "key1") == std::nullopt);
   REQUIRE(upload_storage_->getState("test_bucket", "key2") == state2);
 }
