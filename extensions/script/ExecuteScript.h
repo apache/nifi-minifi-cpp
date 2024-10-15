c28e2da825024ddb2cb69c52f253b8aa94647d19
Martin Zink <martin.zink@apache.org>
2024-10-15 14:34:17 +0200

    MINIFICPP-2477 Valgrind CI

--- extensions/script/ExecuteScript.h
+++ extensions/script/ExecuteScript.h
@@ -94,6 +94,9 @@ class ExecuteScript : public core::Processor {
   void initialize() override;
   void onSchedule(core::ProcessContext& context, core::ProcessSessionFactory& session_factory) override;
   void onTrigger(core::ProcessContext& context, core::ProcessSession& session) override;
+  void notifyStop() override {
+    script_executor_.reset();
+  }
 
  private:
   std::shared_ptr<core::logging::Logger> logger_ = core::logging::LoggerFactory<ExecuteScript>::getLogger(uuid_);
