0344787c3044fb66d1bd240db400dae6086958da
Martin Zink <martinzink@apache.org>
2024-12-11 15:32:01 +0100

    MINIFICPP-2500 Log runtime as human readable duration

--- libminifi/test/unit/TimeUtilTests.cpp
+++ libminifi/test/unit/TimeUtilTests.cpp
@@ -342,3 +342,15 @@ TEST_CASE("Parse RFC3339", "[parseRfc3339]") {
   CHECK_FALSE(parseRfc3339(" 2023-03-01T19:04:55Z"));
   CHECK_FALSE(parseRfc3339("2023-03-01"));
 }
+
+TEST_CASE("Test human readable parser") {
+  using utils::timeutils::humanReadableDuration;
+  CHECK(humanReadableDuration(1234567us) == "1.23s");
+  CHECK(humanReadableDuration(52ms) == "52.00ms");
+
+  CHECK(humanReadableDuration(1000s) == "00:16:40");
+  CHECK(humanReadableDuration(10000s) == "02:46:40");
+  CHECK(humanReadableDuration(2222222222ms) == "25 days, 17:17:02");
+  CHECK(humanReadableDuration(24h) == "1 day, 00:00:00");
+  CHECK(humanReadableDuration(23h + 12min + 1233ms) == "23:12:01");
+}
