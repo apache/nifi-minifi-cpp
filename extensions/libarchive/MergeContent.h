5c5fc8946d7e4a12125c4a9b494adc08fb4d99b3
Martin Zink <martinzink@apache.org>
2024-12-29 21:00:36 +0100

    MINIFICPP-2504 MergeContent should be single threaded
    Closes #1911
    
    Signed-off-by: Marton Szasz <szaszm@apache.org>

--- extensions/libarchive/MergeContent.h
+++ extensions/libarchive/MergeContent.h
@@ -370,7 +370,7 @@ class MergeContent : public processors::BinFiles {
   EXTENSIONAPI static constexpr bool SupportsDynamicProperties = false;
   EXTENSIONAPI static constexpr bool SupportsDynamicRelationships = false;
   EXTENSIONAPI static constexpr core::annotation::Input InputRequirement = core::annotation::Input::INPUT_REQUIRED;
-  EXTENSIONAPI static constexpr bool IsSingleThreaded = false;
+  EXTENSIONAPI static constexpr bool IsSingleThreaded = true;
 
   ADD_COMMON_VIRTUAL_FUNCTIONS_FOR_PROCESSORS
 
