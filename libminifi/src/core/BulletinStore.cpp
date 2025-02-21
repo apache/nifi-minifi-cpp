5eded8e02997621211824ebe2b8ecc03b340c9a8
Gabor Gyimesi <gamezbird@gmail.com>
2025-02-21 15:37:11 +0100

    MINIFICPP-2448 Add minifi.sh flowStatus command

--- libminifi/src/core/BulletinStore.cpp
+++ libminifi/src/core/BulletinStore.cpp
@@ -68,6 +68,17 @@ std::deque<Bulletin> BulletinStore::getBulletins(std::optional<std::chrono::syst
   return {};
 }
 
+std::vector<Bulletin> BulletinStore::getBulletinsForProcessor(const std::string& processor_uuid) const {
+  std::lock_guard<std::mutex> lock(mutex_);
+  std::vector<Bulletin> bulletins;
+  for (const auto& bulletin : bulletins_) {
+    if (bulletin.source_id == processor_uuid) {
+      bulletins.push_back(bulletin);
+    }
+  }
+  return bulletins;
+}
+
 size_t BulletinStore::getMaxBulletinCount() const {
   std::lock_guard<std::mutex> lock(mutex_);
   return max_bulletin_count_;
