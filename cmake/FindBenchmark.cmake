d6451f19c59441fdf763c497481acd1d8f9d0814
Adam Debreceni <64783590+adamdebreceni@users.noreply.github.com>
2025-04-07 10:32:47 +0200

    Update cmake/FindBenchmark.cmake
    Co-authored-by: Martin Zink <martin.zink@protonmail.com>

--- cmake/FindBenchmark.cmake
+++ cmake/FindBenchmark.cmake
@@ -22,5 +22,6 @@ FetchContent_Declare(
         benchmark
         URL      https://github.com/google/benchmark/archive/refs/tags/v1.9.1.tar.gz
         URL_HASH SHA256=32131c08ee31eeff2c8968d7e874f3cb648034377dfc32a4c377fa8796d84981
+        OVERRIDE_FIND_PACKAGE
 )
 FetchContent_MakeAvailable(benchmark)
