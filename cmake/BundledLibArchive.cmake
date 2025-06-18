9873a915c4438e1a1c607576ccd440399a509a6f
Gabor Gyimesi <gamezbird@gmail.com>
2025-06-18 13:45:11 +0200

    MINIFICPP-2579 Upgrade libarchive to v3.8.1

--- cmake/BundledLibArchive.cmake
+++ cmake/BundledLibArchive.cmake
@@ -16,20 +16,19 @@
 # under the License.
 
 function(use_bundled_libarchive SOURCE_DIR BINARY_DIR)
-    # Define patch step
     set(PC "${Patch_EXECUTABLE}" -p1 -i "${SOURCE_DIR}/thirdparty/libarchive/libarchive.patch")
 
-    # Define byproducts
     if (WIN32)
-        set(BYPRODUCT "lib/archive_static.lib")
+        set(BYPRODUCT "lib/archive.lib")
     else()
         set(BYPRODUCT "lib/libarchive.a")
     endif()
 
-    # Set build options
     set(LIBARCHIVE_CMAKE_ARGS ${PASSTHROUGH_CMAKE_ARGS}
             "-DCMAKE_INSTALL_PREFIX=${BINARY_DIR}/thirdparty/libarchive-install"
+            -DCMAKE_INSTALL_LIBDIR=lib
             -DLIBARCHIVE_STATIC=1
+            -DBUILD_SHARED_LIBS=OFF
             -DENABLE_MBEDTLS=OFF
             -DENABLE_NETTLE=OFF
             -DENABLE_LIBB2=OFF
@@ -48,7 +47,8 @@ function(use_bundled_libarchive SOURCE_DIR BINARY_DIR)
             -DENABLE_ICONV=OFF
             -DENABLE_TEST=OFF
             -DENABLE_WERROR=OFF
-            -DENABLE_OPENSSL=ON)
+            -DENABLE_OPENSSL=ON
+            -DENABLE_UNZIP=OFF)
 
     if (NOT ENABLE_LZMA)
         list(APPEND LIBARCHIVE_CMAKE_ARGS -DENABLE_LZMA=OFF)
@@ -64,11 +64,10 @@ function(use_bundled_libarchive SOURCE_DIR BINARY_DIR)
 
     append_third_party_passthrough_args(LIBARCHIVE_CMAKE_ARGS "${LIBARCHIVE_CMAKE_ARGS}")
 
-    # Build project
     ExternalProject_Add(
             libarchive-external
-            URL "https://github.com/libarchive/libarchive/releases/download/v3.4.2/libarchive-3.4.2.tar.gz"
-            URL_HASH "SHA256=b60d58d12632ecf1e8fad7316dc82c6b9738a35625746b47ecdcaf4aed176176"
+            URL "https://github.com/libarchive/libarchive/releases/download/v3.8.1/libarchive-3.8.1.tar.gz"
+            URL_HASH "SHA256=bde832a5e3344dc723cfe9cc37f8e54bde04565bfe6f136bc1bd31ab352e9fab"
             SOURCE_DIR "${BINARY_DIR}/thirdparty/libarchive-src"
             LIST_SEPARATOR % # This is needed for passing semicolon-separated lists
             CMAKE_ARGS ${LIBARCHIVE_CMAKE_ARGS}
@@ -79,7 +78,6 @@ function(use_bundled_libarchive SOURCE_DIR BINARY_DIR)
             TLS_VERIFY TRUE
     )
 
-    # Set dependencies
     add_dependencies(libarchive-external ZLIB::ZLIB OpenSSL::Crypto)
     if (ENABLE_LZMA)
         add_dependencies(libarchive-external LibLZMA::LibLZMA)
@@ -88,24 +86,25 @@ function(use_bundled_libarchive SOURCE_DIR BINARY_DIR)
         add_dependencies(libarchive-external BZip2::BZip2)
     endif()
 
-    # Set variables
     set(LIBARCHIVE_FOUND "YES" CACHE STRING "" FORCE)
     set(LIBARCHIVE_INCLUDE_DIRS "${BINARY_DIR}/thirdparty/libarchive-install/include" CACHE STRING "" FORCE)
     set(LIBARCHIVE_LIBRARY "${BINARY_DIR}/thirdparty/libarchive-install/${BYPRODUCT}" CACHE STRING "" FORCE)
     set(LIBARCHIVE_LIBRARIES ${LIBARCHIVE_LIBRARY} CACHE STRING "" FORCE)
 
-    # Create imported targets
     add_library(LibArchive::LibArchive STATIC IMPORTED)
     set_target_properties(LibArchive::LibArchive PROPERTIES IMPORTED_LOCATION "${LIBARCHIVE_LIBRARY}")
     add_dependencies(LibArchive::LibArchive libarchive-external)
-    set_property(TARGET LibArchive::LibArchive APPEND PROPERTY INTERFACE_LINK_LIBRARIES ZLIB::ZLIB OpenSSL::Crypto)
+    target_link_libraries(LibArchive::LibArchive INTERFACE ZLIB::ZLIB OpenSSL::Crypto)
     if (ENABLE_LZMA)
-        set_property(TARGET LibArchive::LibArchive APPEND PROPERTY INTERFACE_LINK_LIBRARIES LibLZMA::LibLZMA)
+        target_link_libraries(LibArchive::LibArchive INTERFACE LibLZMA::LibLZMA)
     endif()
     if (ENABLE_BZIP2)
-        set_property(TARGET LibArchive::LibArchive APPEND PROPERTY INTERFACE_LINK_LIBRARIES BZip2::BZip2)
+        target_link_libraries(LibArchive::LibArchive INTERFACE BZip2::BZip2)
     endif()
     file(MAKE_DIRECTORY ${LIBARCHIVE_INCLUDE_DIRS})
-    set_property(TARGET LibArchive::LibArchive APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${LIBARCHIVE_INCLUDE_DIRS})
-    set_property(TARGET LibArchive::LibArchive APPEND PROPERTY INTERFACE_COMPILE_DEFINITIONS "LIBARCHIVE_STATIC=1")
+    target_include_directories(LibArchive::LibArchive INTERFACE ${LIBARCHIVE_INCLUDE_DIRS})
+    target_compile_definitions(LibArchive::LibArchive INTERFACE "LIBARCHIVE_STATIC=1")
+    if (WIN32)
+        target_link_libraries(LibArchive::LibArchive INTERFACE xmllite)
+    endif()
 endfunction(use_bundled_libarchive)
