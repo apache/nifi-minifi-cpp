3e526354a743c79431f020229bb96b8eadf01e27
Gabor Gyimesi <gamezbird@gmail.com>
2025-08-05 16:42:18 +0200

    MINIFICPP-2601 Separate protobuf library from grpc
    - Upgrade grpc library to v1.72.2

--- cmake/Grpc.cmake
+++ cmake/Grpc.cmake
@@ -15,6 +15,8 @@
 # specific language governing permissions and limitations
 # under the License.
 include(FetchContent)
+include(Abseil)
+include(Protobuf)
 
 set(gRPC_BUILD_GRPC_CSHARP_PLUGIN OFF CACHE BOOL "" FORCE)
 set(gRPC_BUILD_GRPC_NODE_PLUGIN OFF CACHE BOOL "" FORCE)
@@ -26,20 +28,17 @@ set(RE2_BUILD_TESTING OFF CACHE BOOL "" FORCE)
 set(gRPC_ABSL_PROVIDER "package" CACHE STRING "" FORCE)
 set(gRPC_ZLIB_PROVIDER "package" CACHE STRING "" FORCE)
 set(gRPC_SSL_PROVIDER "package" CACHE STRING "" FORCE)
-set(protobuf_BUILD_TESTS OFF CACHE BOOL "" FORCE)
-set(protobuf_ABSL_PROVIDER "package" CACHE STRING "" FORCE)
+set(gRPC_PROTOBUF_PROVIDER "package" CACHE STRING "" FORCE)
 
-set(PATCH_FILE_1 "${CMAKE_SOURCE_DIR}/thirdparty/grpc/remove-custom-commands-for-unused-dependencies.patch")
-set(PATCH_FILE_2 "${CMAKE_SOURCE_DIR}/thirdparty/grpc/fix-constants-on-windows.patch")
+set(PATCH_FILE "${CMAKE_SOURCE_DIR}/thirdparty/grpc/fix-protobuf-find-package.patch")
 set(PC ${Bash_EXECUTABLE}  -c "set -x &&\
-            (\\\"${Patch_EXECUTABLE}\\\" -p1 -R -s -f --dry-run -i \\\"${PATCH_FILE_1}\\\" || \\\"${Patch_EXECUTABLE}\\\" -p1 -N -i \\\"${PATCH_FILE_1}\\\") &&\
-            (\\\"${Patch_EXECUTABLE}\\\" -p1 -R -s -f --dry-run -i \\\"${PATCH_FILE_2}\\\" || \\\"${Patch_EXECUTABLE}\\\" -p1 -N -i \\\"${PATCH_FILE_2}\\\")")
+            (\\\"${Patch_EXECUTABLE}\\\" -p1 -R -s -f --dry-run -i \\\"${PATCH_FILE}\\\" || \\\"${Patch_EXECUTABLE}\\\" -p1 -N -i \\\"${PATCH_FILE}\\\")")
 
 FetchContent_Declare(
   grpc
   GIT_REPOSITORY https://github.com/grpc/grpc
-  GIT_TAG        v1.68.0
-  GIT_SUBMODULES "third_party/cares/cares third_party/protobuf third_party/re2 third_party/upb"
+  GIT_TAG        v1.72.2
+  GIT_SUBMODULES "third_party/cares/cares third_party/re2 third_party/upb"
   PATCH_COMMAND "${PC}"
 )
 set(FETCHCONTENT_QUIET OFF)
@@ -48,6 +47,4 @@ FetchContent_MakeAvailable(grpc)
 add_dependencies(grpc++ OpenSSL::SSL OpenSSL::Crypto ZLIB::ZLIB)
 
 set(GRPC_INCLUDE_DIR "${grpc_SOURCE_DIR}/include" CACHE STRING "" FORCE)
-set(PROTOBUF_INCLUDE_DIR "${protobuf_SOURCE_DIR}/src" CACHE STRING "" FORCE)
-set(PROTOBUF_COMPILER "$<TARGET_FILE:protoc>" CACHE STRING "" FORCE)
 set(GRPC_CPP_PLUGIN "$<TARGET_FILE:grpc_cpp_plugin>" CACHE STRING "" FORCE)
