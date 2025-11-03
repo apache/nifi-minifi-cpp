5ccec06060c7dee32fd2688d34f831d386024ff8
Adam Debreceni <adebreceni@apache.org>
2025-11-03 06:46:14 +0100

    MINIFICPP-2657 - Fix build

--- extensions/python/tests/PythonManifestTests.cpp
+++ extensions/python/tests/PythonManifestTests.cpp
@@ -164,7 +164,7 @@ class MyPyProc5(FlowFileTransform):
 )";
 
   controller.configuration_->set(minifi::Configuration::nifi_python_processor_dir, python_dir.string());
-#ifdef __linux__
+#ifndef WIN32
   controller.configuration_->set(minifi::Configuration::nifi_extension_path, "*minifi-python-lib-loader*, *minifi-python-script*");
 #else
   controller.configuration_->set(minifi::Configuration::nifi_extension_path, "*minifi-python-script*");
