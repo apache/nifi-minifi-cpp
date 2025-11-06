1b37c3f656222892a50213ee8cb60a330d41e86b
Gabor Gyimesi <gamezbird@gmail.com>
2025-11-06 17:06:18 +0100

    MINIFICPP-2661 Update version number to 1.0.0
    Closes #2058
    
    Signed-off-by: Marton Szasz <szaszm@apache.org>

--- conanfile.py
+++ conanfile.py
@@ -14,7 +14,7 @@ shared_sources = ("CMakeLists.txt", "libminifi/*", "extensions/*", "minifi_main/
 
 class MiNiFiCppMain(ConanFile):
     name = "minifi-cpp"
-    version = "0.99.2"
+    version = "1.0.0"
     license = "Apache-2.0"
     requires = shared_requires
     settings = "os", "compiler", "build_type", "arch"
