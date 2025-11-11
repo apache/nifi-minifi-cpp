d4885471ea7dbe6f316e18488d5fa2c25af3df9f
Gabor Gyimesi <gamezbird@gmail.com>
2025-11-11 10:20:13 +0100

    MINIFICPP-2670 Clean up volatile repositories
    - Remove unused configuration options
    - Remove VolatileFlowFileRepository and make it an alias for NoOpThreadedRepository
    - Remove LegacyVolatileContentRepository
    - Update documentation

--- libminifi/src/core/RepositoryFactory.cpp
+++ libminifi/src/core/RepositoryFactory.cpp
@@ -21,8 +21,8 @@
 #include "core/repository/VolatileContentRepository.h"
 #include "core/ClassLoader.h"
 #include "core/repository/FileSystemRepository.h"
-#include "core/repository/VolatileFlowFileRepository.h"
 #include "core/repository/VolatileProvenanceRepository.h"
+#include "core/repository/NoOpThreadedRepository.h"
 
 using namespace std::literals::chrono_literals;
 
@@ -57,40 +57,6 @@ std::unique_ptr<core::ContentRepository> createContentRepository(const std::stri
   throw std::runtime_error("Support for the provided configuration class could not be found");
 }
 
-class NoOpThreadedRepository : public core::ThreadedRepositoryImpl {
- public:
-  explicit NoOpThreadedRepository(std::string_view repo_name)
-    : ThreadedRepositoryImpl(repo_name) {
-  }
-
-  NoOpThreadedRepository(NoOpThreadedRepository&&) = delete;
-  NoOpThreadedRepository(const NoOpThreadedRepository&) = delete;
-  NoOpThreadedRepository& operator=(NoOpThreadedRepository&&) = delete;
-  NoOpThreadedRepository& operator=(const NoOpThreadedRepository&) = delete;
-
-  ~NoOpThreadedRepository() override {
-    stop();
-  }
-
-  uint64_t getRepositorySize() const override {
-    return 0;
-  }
-
-  uint64_t getRepositoryEntryCount() const override {
-    return 0;
-  }
-
- private:
-  void run() override {
-  }
-
-  std::thread& getThread() override {
-    return thread_;
-  }
-
-  std::thread thread_;
-};
-
 std::unique_ptr<core::Repository> createRepository(const std::string& configuration_class_name, const std::string& repo_name) {
   std::string class_name_lc = configuration_class_name;
   std::transform(class_name_lc.begin(), class_name_lc.end(), class_name_lc.begin(), ::tolower);
@@ -101,12 +67,10 @@ std::unique_ptr<core::Repository> createRepository(const std::string& configurat
     return return_obj;
   }
   // if the desired repos don't exist, we can try doing string matches and rely on volatile repositories
-  if (class_name_lc == "flowfilerepository" || class_name_lc == "volatileflowfilerepository") {
-    return instantiate<repository::VolatileFlowFileRepository>(repo_name);
+  if (class_name_lc == "flowfilerepository" || class_name_lc == "volatileflowfilerepository" || class_name_lc == "nooprepository") {
+    return std::make_unique<repository::NoOpThreadedRepository>(repo_name);
   } else if (class_name_lc == "provenancerepository" || class_name_lc == "volatileprovenancerepository") {
     return instantiate<repository::VolatileProvenanceRepository>(repo_name);
-  } else if (class_name_lc == "nooprepository") {
-    return std::make_unique<core::NoOpThreadedRepository>(repo_name);
   }
   return {};
 }
