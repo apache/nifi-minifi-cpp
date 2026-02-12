734bcccda118200471e83feaab3816475839d71a
Martin Zink <martinzink@apache.org>
2026-02-12 14:12:50 +0100

    linter fix

--- behave_framework/src/minifi_test_framework/containers/container.py
+++ behave_framework/src/minifi_test_framework/containers/container.py
@@ -14,7 +14,7 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #
-from typing import TYPE_CHECKING, Union
+from typing import TYPE_CHECKING
 
 import json
 import logging
