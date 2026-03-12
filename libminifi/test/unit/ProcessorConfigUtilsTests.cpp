117a9d14bfb1686025f4bf48314adec4c2e3ae76
Martin Zink <martinzink@apache.org>
2026-03-12 17:45:58 +0100

    typo fix

--- libminifi/test/unit/ProcessorConfigUtilsTests.cpp
+++ libminifi/test/unit/ProcessorConfigUtilsTests.cpp
@@ -166,16 +166,16 @@ TEST_CASE("Parse controller service property") {
   SECTION("Optional controller service property") {
     SECTION("... is valid") {
       REQUIRE(processor->setProperty(property.name, "TestControllerService"));
-      const auto value = utils::parseOptionalControllerService<TestControllerService>(context, property, processor->getUUID());;
+      const auto value = utils::parseOptionalControllerService<TestControllerService>(context, property, processor->getUUID());
       CHECK(value.get() == test_controller_service->getImplementation<TestControllerService>());
     }
     SECTION("... is missing") {
-      const auto value = utils::parseOptionalControllerService<TestControllerService>(context, property, processor->getUUID());;
+      const auto value = utils::parseOptionalControllerService<TestControllerService>(context, property, processor->getUUID());
       CHECK(value == nullptr);
     }
     SECTION("... is blank") {
       REQUIRE(processor->setProperty(property.name, ""));
-      const auto value = utils::parseOptionalControllerService<TestControllerService>(context, property, processor->getUUID());;
+      const auto value = utils::parseOptionalControllerService<TestControllerService>(context, property, processor->getUUID());
       CHECK(value == nullptr);
     }
     SECTION("... is invalid") {
