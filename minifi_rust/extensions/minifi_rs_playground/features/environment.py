48028a1fe8dfa0ece7310359cef211cec4a93a8f
Martin Zink <martinzink@apache.org>
2026-07-27 15:13:07 +0200

    rebase fix

--- minifi_rust/extensions/minifi_rs_playground/features/environment.py
+++ minifi_rust/extensions/minifi_rs_playground/features/environment.py
@@ -16,16 +16,18 @@
 # under the License.
 
 import os
-from typing import List
 
 from minifi_behave.containers.docker_image_builder import DockerImageBuilder
-from minifi_behave.core.hooks import common_after_scenario
-from minifi_behave.core.hooks import common_before_scenario, get_minifi_container_image
+from minifi_behave.core.hooks import (
+    common_after_scenario,
+    common_before_scenario,
+    get_minifi_container_image,
+)
 from minifi_behave.core.minifi_test_context import MinifiTestContext
 
 
 def add_extension_to_minifi_container(
-    extension_name: str, possible_paths: List[str], context: MinifiTestContext
+    extension_name: str, possible_paths: list[str], context: MinifiTestContext
 ):
     new_container_name = f"apacheminificpp:{extension_name}"
     is_windows = os.name == "nt"
