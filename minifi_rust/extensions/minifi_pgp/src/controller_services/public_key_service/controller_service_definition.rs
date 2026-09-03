32fccc917a0bd99aaf1f4705c37b40acbfd6231c
Martin Zink <martinzink@apache.org>
2026-09-03 16:08:11 +0200

    review changes

--- minifi_rust/extensions/minifi_pgp/src/controller_services/public_key_service/controller_service_definition.rs
+++ minifi_rust/extensions/minifi_pgp/src/controller_services/public_key_service/controller_service_definition.rs
@@ -1,3 +1,20 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 use super::PGPPublicKeyService;
 use crate::controller_services::key_file_property::PublicKeyFile;
 use crate::controller_services::key_property::PublicKey;
@@ -8,13 +25,13 @@ use minifi_native::{
 
 pub(crate) const KEYRING_FILE: Property<Option<PublicKeyFile>> = Property::new(
     "Keyring File",
-    "File path to PGP Keyring or Secret Key encoded in binary or ASCII Armor",
+    "File path to PGP Keyring or Public Key encoded in binary or ASCII Armor",
 )
 .supports_expression_language();
 
 pub(crate) const KEYRING: Property<Option<PublicKey>> = Property::new(
     "Keyring",
-    "PGP Keyring or Secret Key encoded in ASCII Armor",
+    "PGP Keyring or Public Key encoded in ASCII Armor",
 )
 .sensitive();
 
